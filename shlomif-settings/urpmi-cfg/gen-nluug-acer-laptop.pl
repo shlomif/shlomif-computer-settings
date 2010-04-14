67e125f9ba6b588550255178b6a0fcb58d6485bf
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2010-04-14 20:33:19 +0000

    Add the NLUUG script generation mirror.

---
+++ shlomif-settings/urpmi-cfg/gen-nluug-acer-laptop.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Template;
+
+my $template = Template->new();
+
+$template->process(
+    "urpmi.cfg.tt2", 
+    {
+        arch => "x86_64",
+        mdv_mirror => "ftp://ftp.nluug.nl/pub/os/Linux/distr/Mandrivalinux",
+    },
+    "$ENV{HOME}/urpmi.cfg",
+);
