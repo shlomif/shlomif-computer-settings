6614a9b9772b438e529fe6454a51cf05b0e707a1
shlomif <shlomif@iglu.org.il>
2010-05-27 16:03:52 +0000

    Add the gen-isoc.

---
+++ shlomif-settings/urpmi-cfg/gen-isoc-i586.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Template;
+
+my $template = Template->new();
+
+$template->process(
+    "urpmi.cfg.tt2", 
+    {
+        arch => "i586",
+        mdv_mirror => "ftp://mirror.isoc.org.il/mandriva",
+    },
+    "$ENV{HOME}/urpmi.cfg",
+);
