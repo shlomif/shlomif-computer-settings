a9bcaf219c57e241103191cf32ab05f26520b145
shlomif <shlomif@iglu.org.il>
2010-10-18 20:57:33 +0000

    Updated with more up-to-date modules from the laptop.
    Some of the scripts were added later or corrected.

--- shlomif-settings/build-scripts/build/gringotts.sh
+++ shlomif-settings/build-scripts/build/gringotts.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-LIBGRG_CFLAGS="-I $HOME/apps/file/libgringotts/include" \
-    LIBGRG_LIBS="-L $HOME/apps/file/libgringotts/lib -lgringotts" \
-    CFLAGS="-g" ./configure --prefix=$HOME/apps/file/gringotts
+CFLAGS="-g" PKG_CONFIG_PATH="$HOME/apps/file/libgringotts/lib/pkgconfig/" \
+    ./configure --prefix=$HOME/apps/file/gringotts
--- shlomif-settings/build-scripts/build/perl/eumm-install-to-apps-perl-modules.sh
+++ shlomif-settings/build-scripts/build/perl/eumm-install-to-apps-perl-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perl Makefile.PL PREFIX="/home/shlomi/apps/perl/modules" INSTALLSITEBIN="\$(SITEPREFIX)/bin" INSTALLSITESCRIPT="\$(SITEPREFIX)/bin" && \
+perl Makefile.PL PREFIX="$HOME/apps/perl/modules" INSTALLSITEBIN="\$(SITEPREFIX)/bin" INSTALLSITESCRIPT="\$(SITEPREFIX)/bin" && \
 make && \
 make test && \
 make install
