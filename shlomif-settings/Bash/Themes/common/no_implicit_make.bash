95e92d899029ce13be5d954dee037d0516b6d976
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2010-10-20 20:28:01 +0000

    Clarified the comment.

--- shlomif-settings/Bash/Themes/common/no_implicit_make.bash
+++ shlomif-settings/Bash/Themes/common/no_implicit_make.bash
@@ -1,3 +1,3 @@
-# This flag makes make not use implicit rules:
+# This flag makes sure that GNU "make" won't use implicit rules:
 # - http://community.livejournal.com/shlomif_tech/39552.html
 export MAKEFLAGS="-r"
