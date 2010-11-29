7a46b89685d196adbb36d9c52823cfe04a9ae42e
shlomif <shlomif@iglu.org.il>
2010-11-29 21:34:55 +0000

    Add the homepage defs.

---
+++ shlomif-settings/Bash/defs/homepage.bash
@@ -0,0 +1,4 @@
+# Homepage uploading logic
+export __HOMEPAGE_REMOTE_PATH="shlomifish@www.heptagon.co.il:shlomifish.org/www_html"
+export __HOMEPAGE_LOCAL_PATH="$HOME/Docs/homepage/to-upload/hp-root"
+export HOMEPAGE_SSH_PATH="$__HOMEPAGE_LOCAL_PATH"
