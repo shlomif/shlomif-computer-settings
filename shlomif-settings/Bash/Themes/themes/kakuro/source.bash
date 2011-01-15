0d2b5e0ca41447b96a226a16d0f604d83c5b9f07
shlomif <shlomif@iglu.org.il>
2011-01-15 14:44:16 +0000

    Add a setup function.

--- shlomif-settings/Bash/Themes/themes/kakuro/source.bash
+++ shlomif-settings/Bash/Themes/themes/kakuro/source.bash
@@ -6,6 +6,14 @@ trunk="$kakuro/trunk"
 solver="$trunk/solver/ruby"
 this="$solver"
 
+setup()
+{
+    mkdir -p "$kakuro"
+    cd "$kakuro"
+    svn co 'https://svn.berlios.de/svnroot/repos/fc-solve/kakuro-cross-sums/trunk'
+    cd $this
+}
+
 cd "$this"
 
 e()
