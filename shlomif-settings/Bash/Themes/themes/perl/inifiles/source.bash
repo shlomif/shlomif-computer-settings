675884f2f8304e3011a2a4ba57a8b65925e47289
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2011-01-29 10:44:24 +0000

    Add a setup to the config-inifiles theme.

--- shlomif-settings/Bash/Themes/themes/perl/inifiles/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/inifiles/source.bash
@@ -10,6 +10,14 @@ this="$module"
 
 cd "$this"
 
+setup()
+{
+    mkdir -p "$base"
+    cd "$base"
+    svn co 'https://config-inifiles.svn.sourceforge.net/svnroot/config-inifiles/trunk/'
+    cd "$trunk"
+}
+
 t()
 {
     cd "$this" && ./Build runtest
