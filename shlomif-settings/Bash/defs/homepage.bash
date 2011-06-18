f778d55fcb223053c0c1b7d8bc90903138edc0d9
shlomif <shlomif@iglu.org.il>
2011-06-18 14:16:23 +0000

    Convert to a new path.

--- shlomif-settings/Bash/defs/homepage.bash
+++ shlomif-settings/Bash/defs/homepage.bash
@@ -1,4 +1,5 @@
 # Homepage uploading logic
-export __HOMEPAGE_REMOTE_PATH="shlomifish@www.heptagon.co.il:shlomifish.org/www_html"
+# export __HOMEPAGE_REMOTE_PATH="shlomi.fish@www.heptagon.co.il:shlomifish.org/www_html"
+export __HOMEPAGE_REMOTE_PATH="hostgator:public_html/"
 export __HOMEPAGE_LOCAL_PATH="$HOME/Docs/homepage/to-upload/hp-root"
 export HOMEPAGE_SSH_PATH="$__HOMEPAGE_LOCAL_PATH"
