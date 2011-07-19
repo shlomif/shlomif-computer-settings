4bbb9604ef5f107cb1d74e2654d5763ab1664d22
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2011-07-19 15:41:28 +0000

    Add more aliases.

--- shlomif-settings/bash-aliases/user_aliases.bash
+++ shlomif-settings/bash-aliases/user_aliases.bash
@@ -1,1 +1,29 @@
 alias q='urpmq --fuzzy -a'
+alias myrsync='rsync -a --progress -v --inplace'
+
+dc()
+{
+    echo "It's cd - not dc dammit!" 1>&2
+}
+
+disable_local_lib()
+{
+    unset MODULEBUILDRC PERL_MM_OPT
+}
+alias screen='screen -e "^^6"'
+alias rem_up='HOMEPAGE_SSH_PATH="$__HOMEPAGE_REMOTE_PATH" make upload'
+alias perlbug='perlbug -r shlomif@iglu.org.il -c shlomif@iglu.org.il -e "gvim -f"'
+mdvsys()
+{
+    (
+        disable_local_lib
+        `which mdvsys` "$@"
+    )
+}
+rpmbuild()
+{
+    (
+        disable_local_lib
+        `which rpmbuild` "$@"
+    )
+}
