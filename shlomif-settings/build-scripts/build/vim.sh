d1ce137fba1763f885e15f4f81865e74385d928b
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2011-07-19 21:33:47 +0000

    vim build script now accepts optional arguments.

--- shlomif-settings/build-scripts/build/vim.sh
+++ shlomif-settings/build-scripts/build/vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./configure --prefix=$HOME/apps/vim --with-features=huge \
+set -x
+./configure "$@" --prefix=$HOME/apps/vim --with-features=huge \
     --enable-perlinterp --enable-pythoninterp --enable-rubyinterp \
     --enable-tclinterp --enable-luainterp
