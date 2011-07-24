4d87bf32cc6eb7547a426ebfb8a5b2920635cd4d
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2011-07-24 11:19:00 +0000

    Convert the "nurikabe" theme to "load_common hg".

--- shlomif-settings/Bash/Themes/themes/nurikabe/source.bash
+++ shlomif-settings/Bash/Themes/themes/nurikabe/source.bash
@@ -1,11 +1,14 @@
 load_common mymake
 load_common completion
 load_common prompt
+load_common hg
 
 base="$HOME/progs/games/nurikabe"
+hg_base="$base"
 trunk="$base/trunk"
 c_src="$trunk/nurikabe-solver"
 module="$c_src/Games-Nurikabe-Solver"
+remote_repo='ssh://hg@bitbucket.org/shlomif/nurikabe'
 
 this="$module"
 
