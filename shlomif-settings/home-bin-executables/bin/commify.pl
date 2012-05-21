f9242660d3090568f758afbc521b6ed70a0e294a
shlomif <shlomif@iglu.org.il>
2012-05-21 20:34:48 +0000

    Add the commify script.

---
+++ shlomif-settings/home-bin-executables/bin/commify.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use IO::Handle;
+
+sub commify {
+    my ( $sign, $int, $frac ) = ( $_[0] =~ /^([+-]?)(\d*)(.*)/ );
+    my $commified = (
+        scalar reverse join ',',
+        unpack '(A3)*',
+        scalar reverse $int
+    );
+    return $sign . $commified . $frac;
+}
+
+sub multi_commify
+{
+    my $l = shift;
+
+    $l =~ s/(\d+(?:\.\d+)?)/commify($1)/ge;
+
+    return $l
+}
+
+STDOUT->autoflush(1);
+
+while (my $l = <STDIN>)
+{
+    print multi_commify($l);
+}
