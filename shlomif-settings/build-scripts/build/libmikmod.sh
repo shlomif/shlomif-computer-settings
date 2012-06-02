5f46de6a76d620bb6c61e2442ed2b1947cc8b5a5
shlomif <shlomif@iglu.org.il@7334f695-59ef-4367-843d-48bbf0183b29>
2012-06-02 10:09:28 +0000

    Fix a duplicate line

--- shlomif-settings/build-scripts/build/libmikmod.sh
+++ shlomif-settings/build-scripts/build/libmikmod.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 CFLAGS="-g" ./configure --prefix=$HOME/apps/multimedia/libmikmod \
-./configure --prefix=$HOME/apps/multimedia/libmikmod \
      --enable-alsa --enable-oss --enable-maintainer-mode
