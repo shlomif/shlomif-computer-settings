683fe3d2bb3a0878cbc6bd6aa1f5c0ef71d61ac0
Shlomi Fish <shlomif@shlomifish.org>
2012-06-28 23:06:59 +0300

    Add libmikmod-cmake.

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -4,5 +4,5 @@ repeat_count="$1"
 shift
 
 for I in $(seq 1 "$repeat_count") ; do
-    ARGS="--worker-step 16 -l eo" bash scripts/time-threads-num.bash 4 4
+    ARGS="--worker-step 16 -l te" bash ../scripts/time-threads-num.bash 4 4
 done
