19b7035422cdfa39a22bd890f646eaa7975621dd
Shlomi Fish <shlomif@shlomifish.org>
2012-06-29 17:19:32 +0300

    Add more.

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -3,6 +3,8 @@
 repeat_count="$1"
 shift
 
+num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"
+
 for I in $(seq 1 "$repeat_count") ; do
-    ARGS="--worker-step 16 -l eo" bash scripts/time-threads-num.bash 4 4
+    ARGS="--worker-step 16 -l eo" bash scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
 done
