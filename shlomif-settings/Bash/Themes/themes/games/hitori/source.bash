62354a1b5d513ee94b7a57e6d7dff7e15ab2762f
Shlomi Fish <shlomif@shlomifish.org>
2012-07-30 00:12:05 +0300

    Convert to the Mercurial repo.

--- shlomif-settings/Bash/Themes/themes/games/hitori/source.bash
+++ shlomif-settings/Bash/Themes/themes/games/hitori/source.bash
@@ -1,11 +1,14 @@
 load_common mymake
 load_common completion
+load_common hg
 
 base="$HOME/progs/games/hitori"
+hg_base="$base"
 trunk="$base/trunk"
 solver="$trunk/hitori-solver-in-ruby"
 this="$solver"
 
+remote_repo='ssh://hg@bitbucket.org/shlomif/hitori'
 repos="https://svn.berlios.de/svnroot/repos/fc-solve/hitori/"
 repos_trunk="${repos}trunk/"
 
