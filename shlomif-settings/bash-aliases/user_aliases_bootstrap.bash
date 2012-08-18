17df7f636ab260f4d6fd17f97e4763f688b41b38
Shlomi Fish <shlomif@shlomifish.org>
2012-08-18 22:05:11 +0300

    Add the common aliases

--- shlomif-settings/bash-aliases/user_aliases_bootstrap.bash
+++ shlomif-settings/bash-aliases/user_aliases_bootstrap.bash
@@ -1,5 +1,8 @@
 _reread_aliases()
 {
+    if ! test -f ~/.common_aliases.bash ; then
+        cp -f ~/conf/trunk/shlomif-settings/bash-aliases/common_aliases.bash ~/.common_aliases.bash
+    fi
     source ~/conf/trunk/shlomif-settings/bash-aliases/user_aliases.bash
 }
 _reread_aliases
