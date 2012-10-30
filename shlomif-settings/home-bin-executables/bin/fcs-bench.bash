9539dc142dae8b39021b302e9e8496f3b37bc715
Shlomi Fish <shlomif@shlomifish.org>
2012-10-30 16:00:01 +0200

    Freecell Solver related executables.

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -6,5 +6,5 @@ shift
 num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"
 
 for I in $(seq 1 "$repeat_count") ; do
-    ARGS="--worker-step 16 -l te" bash scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
+    ARGS="--worker-step 16 -l as" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
 done
