609dfeadcf53517dd6ba38aed64e259702e153d3
Shlomi Fish <shlomif@shlomifish.org>
2012-10-30 15:10:49 +0200

    Build scripts.

--- shlomif-settings/build-scripts/build/pidgin.sh
+++ shlomif-settings/build-scripts/build/pidgin.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pref="$HOME/apps/pidgin-beta"
-./configure --prefix="$pref" --disable-vv --disable-meanwhile --disable-nm --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
+# ./configure --prefix="$pref" --disable-vv --disable-meanwhile --disable-nm --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
+./configure --prefix="$pref" --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
