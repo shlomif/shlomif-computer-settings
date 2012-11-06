24995d8ad6c01c588b08685ccc711eaa1b07935b
Shlomi Fish <shlomif@shlomifish.org>
2012-11-06 19:35:33 +0200

    Rename

--- shlomif-settings/Bash/Themes/themes/games/binarypuzzle/source.bash
+++ shlomif-settings/Bash/Themes/themes/games/binarypuzzle/source.bash
@@ -3,9 +3,9 @@ load_common completion
 load_common prompt
 load_common git
 
-kakuro="$HOME/progs/games/binarypuzzle.com"
-git_base="$kakuro"
-trunk="$kakuro/garden"
+bp="$HOME/progs/games/binarypuzzle.com"
+git_base="$bp"
+trunk="$bp/garden"
 solver="$trunk/ruby"
 this="$solver"
 remote_repo='git@github.com:shlomif/binary-puzzle-garden.git'
@@ -16,7 +16,7 @@ e()
 {
     (
         cd "$this" && \
-        gvim -p t/parse-board.rb kakuro-board.rb
+        gvim -p t/parse-board.rb binary_puzzle_solver.rb
     )
 }
 
@@ -25,7 +25,7 @@ prompt()
     __prompt_cmd \
         "\$solver=$solver" \
         "\$trunk=$trunk" \
-        "\$base=$kakuro" \
+        "\$base=$bp" \
         "~=$HOME"
 }
 
