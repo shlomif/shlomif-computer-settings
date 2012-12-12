08cd9fd114466dd637b09891d7926a1e46acaa2f
Shlomi Fish <shlomif@shlomifish.org>
2012-12-12 15:16:23 +0200

    Restored the Qantor theme.

--- shlomif-settings/Bash/Themes/themes/perl/qantor/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/qantor/source.bash
@@ -1,11 +1,14 @@
 load_common mymake
 load_common completion
 load_common prompt
+load_common hg
 # load_common gen_patch
 
 base="$HOME/progs/perl/cpan/Text/Qantor"
+hg_base="$base"
 trunk="$base/trunk"
 this="$trunk/Text-Qantor"
+remote_repo='ssh://hg@bitbucket.org/shlomif/qantor'
 rw_repos_url="https://svn.berlios.de/svnroot/repos/web-cpan/Text-Qantor"
 read_repos_url="http://svn.berlios.de/svnroot/repos/web-cpan/Text-Qantor"
 
