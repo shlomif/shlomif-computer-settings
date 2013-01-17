bcf74a87f3ddb94a2046ec2aaefe08d4642cecff
Shlomi Fish <shlomif@shlomifish.org>
2013-01-17 16:54:02 +0200

    Some additions for AI-Pathfinding.

--- shlomif-settings/Bash/Themes/themes/fcs/source.bash
+++ shlomif-settings/Bash/Themes/themes/fcs/source.bash
@@ -14,11 +14,16 @@ site="$t_fcs/site/wml"
 blog="$t_fcs/site/blog"
 presets="$t_fcs/presets/soft-threads/meta-moves/auto-gen"
 atomic_presets="$t_fcs/presets/soft-threads/atomic-moves/auto-gen"
-verify="$trunk/Games-Solitaire-Verify/Games-Solitaire-Verify"
+cpan="$trunk/cpan"
+verify="$cpan/Games-Solitaire-Verify/Games-Solitaire-Verify"
 bench="$t_fcs/benchmarks"
 test_dir="$branches/trunk/testing/source"
 dd_branch="$branches/depth-dependent-moves-order/source"
 parallel="$t_fcs/scripts/parallel-solve-and-verify-for-bakers-game"
+
+ai_path="$cpan/AI-Pathfinding-OptimizeMultiple"
+ai_path_sys_tests="$cpan/temp-AI-Pathfinding-OptimizeMultiple-system-tests"
+
 
 this="$c_src"
 # this="$dd_branch"
@@ -67,6 +72,15 @@ make()
     else
         `which make` "$@"
     fi
+}
+
+_dzil_inst()
+{
+    (
+        cd "$ai_path" && \
+            dzil test --release && \
+            dzil install --install-command='bash ~/conf/build/perl/install-to-apps-perl-modules.sh'
+    )
 }
 
 PS1="\\u[fcs]:\$(prompt)\\$ "
