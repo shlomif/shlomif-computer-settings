77cafb75d984bcab5f383536a72d49d80ce4e2b4
Shlomi Fish <shlomif@shlomifish.org>
2013-02-20 23:30:49 +0200

    Add emscripten-perl.
    This is for trying to build Perl using emscripten.

---
+++ shlomif-settings/build-scripts/build/perl/emscripten-perl.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+unlink ("config.sh", "Policy.sh");
+
+system("sh", "Configure", "-de", "-Dprefix=$ENV{HOME}/apps/perl/emcc",
+    "-Dusedevel",
+    "-Dcc=emcc",
+    "-D_o=.bc",
+    q#-Doptimize=-O2 -m32 -std=gnu99#,
+);
