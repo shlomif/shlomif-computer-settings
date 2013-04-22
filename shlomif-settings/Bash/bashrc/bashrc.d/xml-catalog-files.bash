60d2bef6d904f9f5f45804e8b8438964a673801c
Shlomi Fish <shlomif@shlomifish.org>
2013-04-22 12:31:05 +0300

    Add more.

--- shlomif-settings/Bash/bashrc/bashrc.d/xml-catalog-files.bash
+++ shlomif-settings/Bash/bashrc/bashrc.d/xml-catalog-files.bash
@@ -1,2 +1,14 @@
 homepage="$HOME/Docs/homepage/homepage/trunk"
-export XML_CATALOG_FILES="$homepage/rng/catalog.xml $HOME/Download/unpack/prog/web/validator-1.1/htdocs/sgml-lib/catalog.xml /etc/xml/catalog"
+htdocs_base="$HOME/Download/unpack/prog/web"
+htdocs_cat="$htdocs_base/validator-1.1/htdocs/sgml-lib/catalog.xml"
+export XML_CATALOG_FILES="$homepage/rng/catalog.xml $htdocs_cat /etc/xml/catalog"
+
+if ! test -e "$htdocs_cat" ; then
+    mkdir -p "$htdocs_base"
+    cd "$htdocs_base"
+    wget -c http://validator.w3.org/sgml-lib.tar.gz
+    tar -xvf sgml-lib.tar.gz
+fi
+if ! test -e "$htdocs_cat" ; then
+    echo "Cannot install '$htdocs_cat'" 1>&2
+fi
