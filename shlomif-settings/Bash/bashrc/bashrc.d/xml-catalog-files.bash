056adaa0bb2a2e41b300581fc1f6a084688f47ce
Shlomi Fish <shlomif@shlomifish.org>
2013-04-22 20:59:57 +0300

    Add for extracting the docbooks.

--- shlomif-settings/Bash/bashrc/bashrc.d/xml-catalog-files.bash
+++ shlomif-settings/Bash/bashrc/bashrc.d/xml-catalog-files.bash
@@ -12,3 +12,31 @@ fi
 if ! test -e "$htdocs_cat" ; then
     echo "Cannot install '$htdocs_cat'" 1>&2
 fi
+
+(
+    db_xsl_ns_dir="$HOME/Download/unpack/file/docbook"
+    db_xsl_ns_base="docbook-xsl-ns-snapshot"
+    db_xsl_base="docbook-xsl-snapshot"
+    db_xsl_ns_complete="$db_xsl_ns_dir/$db_xsl_ns_base"
+    db_xsl_complete="$db_xsl_ns_dir/$db_xsl_base"
+
+    if ! test -d "$db_xsl_ns_complete" ; then
+        (
+            mkdir -p "$db_xsl_ns_dir"
+            cd "$db_xsl_ns_dir"
+            wget -c http://snapshots.docbook.org/docbook-xsl-ns-snapshot.tar.bz2
+            tar -xvf docbook-xsl-ns-snapshot.tar.bz2
+            rm -f docbook-xsl-ns-snapshot.tar.bz2
+            touch "$db_xsl_ns_complete"/.profile.incl
+        )
+    fi
+    if ! test -d "$db_xsl_complete" ; then
+        (
+            mkdir -p "$db_xsl_ns_dir"
+            cd "$db_xsl_ns_dir"
+            wget -c http://snapshots.docbook.org/docbook-xsl-snapshot.tar.bz2
+            tar -xvf docbook-xsl-snapshot.tar.bz2
+            rm -f docbook-xsl-snapshot.tar.bz2
+        )
+    fi
+)
