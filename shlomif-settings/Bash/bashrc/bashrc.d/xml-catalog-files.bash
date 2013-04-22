090d5ecfa4cd5ab11f7475fd1880297909c3c7f2
Shlomi Fish <shlomif@shlomifish.org>
2013-04-22 12:25:30 +0300

    Add the bashrc.d files.
    These are intended to be sourced from the .bashrc one after the other.

---
+++ shlomif-settings/Bash/bashrc/bashrc.d/xml-catalog-files.bash
@@ -0,0 +1,2 @@
+homepage="$HOME/Docs/homepage/homepage/trunk"
+export XML_CATALOG_FILES="$homepage/rng/catalog.xml $HOME/Download/unpack/prog/web/validator-1.1/htdocs/sgml-lib/catalog.xml /etc/xml/catalog"
