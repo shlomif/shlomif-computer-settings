179d1c46d79a37b584e94bc9ae6e06feebb51d83
Shlomi Fish <shlomif@shlomifish.org>
2013-05-09 13:30:57 +0300

    Add the cpanp-conf.pl file.

--- shlomif-settings/home-bin-executables/bin/cpanp-conf.pl
+++ shlomif-settings/home-bin-executables/bin/cpanp-conf.pl
@@ -3,17 +3,19 @@
 use strict;
 use warnings;
 
+use CPANPLUS::Configure;
+
+my $email = 'shlomif@shlomifish.org';
+
 my $conf = CPANPLUS::Configure->new();
 $conf->set_conf( verbose => 1 );
-$conf->set_conf( cpantest => 'dont_cc' );
-$conf->set_conf( cpantest_mx => $mx ) if $mx;
 $conf->set_conf( email => $email );
 $conf->set_conf( prefer_bin => 1 );
-$conf->set_conf( makeflags => 'UNINST=1' );
-$conf->set_conf( buildflags => 'uninst=1' );
-$conf->set_conf( enable_custom_sources => 0 );
+# $conf->set_conf( enable_custom_sources => 0 );
 $conf->set_conf( show_startup_tip => 0 );
 $conf->set_conf( write_install_logs => 0 );
+$conf->set_conf( allow_build_interactivity => 0 );
+$conf->set_conf( prereqs => 1 );
 
 $conf->save();
 
