8bf104ccd4e6982a318b1c98f5fdb267daeeb404
Shlomi Fish <shlomif@shlomifish.org>
2013-05-20 23:20:46 +0300

    Add the Dev-Path-Configs.
    It contains some paths to set for development.

---
+++ shlomif-settings/home-bin-executables/shlomif-specific/Dev-Path-Configs-Source-Me.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$HOME/apps/multimedia/mikmod/bin:$HOME/apps/multimedia/libmikmod/bin:$HOME/apps/spidermonkey-js/bin:$HOME/Download/unpack/xml/ebookmaker:$HOME/apps/vim/bin:$HOME/apps/llvm/bin/:$HOME/Download/unpack/prog/llvm-to-js/emscripten:$HOME/Download/unpack/prog/ninja/ninja:$PATH"
+
+export LD_LIBRARY_PATH="$HOME/apps/multimedia/libmikmod/lib"
