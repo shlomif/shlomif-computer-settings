730dc9acc02b10fcf01bbb396a97f49e4b1b5a82
Shlomi Fish <shlomif@shlomifish.org>
2013-06-20 23:07:05 +0300

    More stuff to the PATH.

--- shlomif-settings/home-bin-executables/shlomif-specific/Dev-Path-Configs-Source-Me.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/Dev-Path-Configs-Source-Me.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH="$HOME/apps/multimedia/mikmod/bin:$HOME/apps/multimedia/libmikmod/bin:$HOME/apps/spidermonkey-js/bin:$HOME/Download/unpack/xml/ebookmaker:$HOME/apps/vim/bin:$HOME/apps/llvm/bin/:$HOME/Download/unpack/prog/llvm-to-js/emscripten:$HOME/Download/unpack/prog/ninja/ninja:$HOME/apps/fcs-for-pysol/bin/:$PATH"
+export PATH="$HOME/apps/gnome-subtitles/bin:$HOME/apps/multimedia/mikmod/bin:$HOME/apps/multimedia/libmikmod/bin:$HOME/apps/spidermonkey-js/bin:$HOME/Download/unpack/xml/ebookmaker:$HOME/apps/vim/bin:$HOME/apps/llvm/bin/:$HOME/Download/unpack/prog/llvm-to-js/emscripten:$HOME/Download/unpack/prog/ninja/ninja:$HOME/apps/fcs-for-pysol/bin/:$PATH"
 
 export LD_LIBRARY_PATH="$HOME/apps/multimedia/libmikmod/lib"
