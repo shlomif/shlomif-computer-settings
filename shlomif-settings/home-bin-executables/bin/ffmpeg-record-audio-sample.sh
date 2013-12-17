8932451bd075a79dbbdbad3ebc402e23e3d9fbda
Shlomi Fish <shlomif@shlomifish.org>
2013-12-17 20:31:38 +0200

    Shell scripts for cmd-line recording of sound.

---
+++ shlomif-settings/home-bin-executables/bin/ffmpeg-record-audio-sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is an ffmpeg invocation that works on my desktop machine in order
+# to record sound (and only sound - not video). It may or may not work for
+# you. No strings attached.
+ffmpeg -f alsa -i plughw:DEV=0,2 -acodec pcm_s16le -t 20 output.wav
