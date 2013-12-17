122f6e0a65bc424ec9d1ffe325edd303cc63e85f
Shlomi Fish <shlomif@shlomifish.org>
2013-12-17 20:00:39 +0200

    More

--- shlomif-settings/build-scripts/build/KDE/gwenview-debug.sh
+++ shlomif-settings/build-scripts/build/KDE/gwenview-debug.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Choqok is a micro-blogging client for KDE.
-test ! -e build && mkdir build
-cd build
-rm -f CMakeCache.txt
 cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/gwenview" \
     -DCMAKE_BUILD_TYPE=debugfull \
-    ..
+    "$1"
