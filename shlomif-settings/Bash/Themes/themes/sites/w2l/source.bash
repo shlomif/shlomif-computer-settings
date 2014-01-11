67dfd522d28c7c00c89cf0031fff2486fc05023b
Shlomi Fish <shlomif@shlomifish.org>
2014-01-11 12:22:15 +0200

    Add the w2l source.

--- shlomif-settings/Bash/Themes/themes/sites/w2l/source.bash
+++ shlomif-settings/Bash/Themes/themes/sites/w2l/source.bash
@@ -4,6 +4,16 @@ load_common completion
 base="$HOME/Docs/IGLU/Welcome-to-Linux"
 trunk="$base/trunk"
 
+setup()
+{
+    (
+    if ! test -e "$trunk" ; then
+        mkdir -p "$base"
+        cd "$base"
+        svn checkout 'https://linux-israel.googlecode.com/svn/welcome-to-linux/site/trunk/'
+    fi
+    )
+}
 this="$trunk"
 cd $this
 
