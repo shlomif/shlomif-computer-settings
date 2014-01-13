880aa7386a7a1b4efe2f8b98c6adcb24fe791729
Shlomi Fish <shlomif@shlomifish.org>
2014-01-13 11:38:47 +0200

    replace with a symlink.
    We no longer use games-solitaire-verify.sh.

--- shlomif-settings/build-scripts/build/perl/install-to-apps-perl-modules.sh
+++ shlomif-settings/build-scripts/build/perl/install-to-apps-perl-modules.sh
@@ -1,1 +1,23 @@
-games-solitaire-verify.sh
+#!/bin/bash
+
+b_pl="Build.PL"
+prefix="$HOME/apps/perl/modules"
+
+if -e "$b_pl" ; then
+    (
+        unset PERL_MM_OPT
+        unset MODULEBUILDRC
+        args="--prefix=$prefix"
+        perl "$b_pl" $args && \
+            ./Build build $args && \
+            ./Build test $args && \
+            ./Build install $args
+    )
+else
+    (
+        perl Makefile.PL PREFIX="$prefix" INSTALLSITEBIN="\$(SITEPREFIX)/bin" INSTALLSITESCRIPT="\$(SITEPREFIX)/bin" && \
+        make && \
+        make test && \
+        make install
+    )
+fi
