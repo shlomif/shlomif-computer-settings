6e7ce2868e8eaf6b9aaa27ea479ab9f531bac6f7
Shlomi Fish <shlomif@shlomifish.org>
2014-01-29 17:13:43 +0200

    More to the build-libxmp

--- shlomif-settings/build-scripts/build/libxmp/build-libxmp-for-emscripten.bash
+++ shlomif-settings/build-scripts/build/libxmp/build-libxmp-for-emscripten.bash
@@ -3,10 +3,11 @@ jslib="libxmp.js"
 autoconf && \
 emconfigure ./configure && \
 perl -i.bak -ple 's/\ALOBJS\s*=.*/LOBJS = \$(OBJS)/' Makefile && \
+TAB=$'\t'
 cat <<EOF >> Makefile && \
 
 ${jslib}: \$(OBJS)
-	emcc -O2 \$(OBJS) -o \$@
+${TAB}emcc --jcache -s TOTAL_MEMORY="$((128 * 1024 * 1024 ))"-O2 \$(OBJS) -o \$@
 
 EOF
 emmake make -j4 "${jslib}"
