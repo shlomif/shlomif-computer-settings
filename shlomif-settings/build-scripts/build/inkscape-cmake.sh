921a5dbbad7b4bd6403f8c6bf4f515edb8c81b2c
Shlomi Fish <shlomif@shlomifish.org>
2014-04-24 20:55:21 +0300

    ENABLE_LCMS=ON + CMAKE_BUILD_TYPE=Debug.

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 build_type=''
 if test "$DEBUG" = 1 ; then
-    build_type='-DCMAKE_BUILD_TYPE=DebugFull'
+    build_type='-DCMAKE_BUILD_TYPE=Debug'
 fi
-cmake -G Ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=OFF -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=OFF -DENABLE_POPPLER_CAIRO=OFF ../inkscape/
+cmake -G Ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=OFF -DENABLE_POPPLER_CAIRO=OFF ../inkscape/
