a1a9964bf59b7250a76679dd89a176b3a5ba64bd
Shlomi Fish <shlomif@shlomifish.org>
2014-04-24 17:56:37 +0300

    Add DEBUG flag

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=OFF -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=OFF -DENABLE_POPPLER_CAIRO=OFF ../inkscape/
+build_type=''
+if test "$DEBUG" = 1 ; then
+    build_type='-DCMAKE_BUILD_TYPE=DebugFull'
+fi
+cmake -G Ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=OFF -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=OFF -DENABLE_POPPLER_CAIRO=OFF ../inkscape/
