c613c687b97402f8209e9b19e7867d0ff78f0a42
Shlomi Fish <shlomif@shlomifish.org>
2014-05-08 20:25:15 +0300

    Add more optimisation flags

--- shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
+++ shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -f config.sh Policy.sh
-sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Dusedevel
+sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Doptimize='-O3 -flto -march=native' -Dusedevel
