1c54d29923848cc6d956334bec85b0cd2d3b585f
Shlomi Fish <shlomif@shlomifish.org>
2014-05-16 12:09:48 +0300

    [Bash Themes] Move away some unused functions.
    From perl/core.

--- shlomif-settings/Bash/Themes/themes/perl/core/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/core/source.bash
@@ -8,33 +8,6 @@ this="$p5_base/git/perl"
 
 cd "$this"
 
-function edit {
-    if [ ! -e "$1.orig" ] ; then
-        cp "$1" "$1.orig"
-    fi
-    gvim -f "$1"
-}
-
-function unedit {
-if [ -L "$1".orig ]; then
-    rm "$1"
-    mv "$1".orig "$1"
-fi
-}
-
-mkpatchorig() {
-    local diffopts
-    for f in `find . -name '*.orig' | sed s,^\./,,`
-    do
-        case `echo $f | sed 's,.orig$,,;s,.*\.,,'` in
-            c)   diffopts=-p ;;
-            pod) diffopts='-F^=' ;;
-            *)   diffopts= ;;
-        esac
-        diff -du $diffopts $f `echo $f | sed 's,.orig$,,'`
-    done
-}
-
 _build_generic()
 {
     script="$1"
