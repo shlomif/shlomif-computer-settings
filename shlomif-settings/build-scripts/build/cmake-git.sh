25ba9e17d11af6eec3c825286f1b9618a22f1a85
Shlomi Fish <shlomif@shlomifish.org>
2014-10-25 13:48:33 +0300

    add a theme.

--- shlomif-settings/build-scripts/build/cmake-git.sh
+++ shlomif-settings/build-scripts/build/cmake-git.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if ! test -e build ; then
-    mkdir build
-fi
-cd build
-cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/cmake-git" ..
+cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/cmake-git" ../gwenview
