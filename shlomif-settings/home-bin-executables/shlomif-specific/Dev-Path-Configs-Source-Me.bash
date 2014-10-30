96700848c1a4c2536215bb18ec157c38593adbef
Shlomi Fish <shlomif@shlomifish.org>
2014-10-30 21:40:37 +0200

    new emscripten paths.

--- shlomif-settings/home-bin-executables/shlomif-specific/Dev-Path-Configs-Source-Me.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/Dev-Path-Configs-Source-Me.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="$HOME/apps/gnome-subtitles/bin:$HOME/apps/multimedia/mikmod/bin:$HOME/apps/multimedia/libmikmod/bin:$HOME/apps/spidermonkey-js/bin:$HOME/Download/unpack/xml/ebookmaker:$HOME/apps/vim/bin:$HOME/apps/llvm/bin/:$HOME/Download/unpack/prog/llvm-to-js/emscripten:$HOME/Download/unpack/prog/ninja/ninja:$HOME/apps/fcs-for-pysol/bin/:$PATH"
+export PATH="$HOME/Download/unpack/prog/llvm-to-js/emsdk_portable:$HOME/Download/unpack/prog/llvm-to-js/emsdk_portable/clang/fastcomp/build_master_64/bin:$HOME/Download/unpack/prog/llvm-to-js/emsdk_portable/emscripten/master:$HOME/apps/gnome-subtitles/bin:$HOME/apps/multimedia/mikmod/bin:$HOME/apps/multimedia/libmikmod/bin:$HOME/apps/spidermonkey-js/bin:$HOME/Download/unpack/xml/ebookmaker:$HOME/apps/vim/bin:$HOME/Download/unpack/prog/ninja/ninja:$HOME/apps/fcs-for-pysol/bin/:$PATH"
 
 export LD_LIBRARY_PATH="$HOME/apps/xml/libxml2/lib/"
 export PKG_CONFIG_PATH="$HOME/apps/file/libgringotts/lib/pkgconfig"
