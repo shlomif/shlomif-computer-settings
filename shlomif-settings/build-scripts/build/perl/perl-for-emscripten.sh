e49749da17c88bd003b947a521c5c7859696c6e1
Shlomi Fish <shlomif@shlomifish.org>
2014-10-30 21:58:30 +0200

    Build is successful.

--- shlomif-settings/build-scripts/build/perl/perl-for-emscripten.sh
+++ shlomif-settings/build-scripts/build/perl/perl-for-emscripten.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -f config.sh Policy.sh
 # -flto does not work properly in gcc-4.9.x without -ffat-lto-objects
-sh Configure -de -Dprefix=/usr -Doptimize='-g' -Dusedevel -Uuse64bitint -Uuse64bitall -Ud_sem -Di_fcntl
+sh Configure -de -Dprefix=/usr -Doptimize='-g' -Dusedevel -Uuse64bitint -Uuse64bitall -Ud_sem -Di_fcntl -Ui_math
+# perl -lape 's#^#// # if m{^#define LIBM_LIB_VERSION}' config.h
