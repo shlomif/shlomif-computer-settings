3377202824da750b34328f04cdab73534f918dd2
Shlomi Fish <shlomif@shlomifish.org>
2014-11-03 12:57:26 +0200

    add more file types.

--- shlomif-settings/home-bin-executables/shlomif-specific/copy-music-files-to-permanent-location.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/copy-music-files-to-permanent-location.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-ls ~shlomif/Download/Video/*.{mp4,flv,webm,mp3,ogg,ogv,ogm,mp2,mpeg,mpg} |
+ls ~shlomif/Download/Video/*.{flv,mkv,mp2,mp3,mp4,mpeg,mpg,ogg,ogm,ogv,webm} |
+sort |
 (
     while read fn
     do
