677e1bdea64f84b965578290b86e00130acfbdba
Shlomi Fish <shlomif@shlomifish.org>
2015-01-07 16:09:42 +0200

    add the configure cpanplus/DWIM.

---
+++ shlomif-settings/home-bin-executables/bin/perl-Configure-CPANPLUS-DWIM.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PERL_AUTOINSTALL=--alldeps
+perl -MCPANPLUS::Configure -e 'my $conf = CPANPLUS::Configure->new(); $conf->set_conf("prereqs" => 1); $conf->save();'
+cpanp -i Term::ReadLine::Gnu KiokuDB KiokuDB::Backend::DBI DBD::SQLite Moose
