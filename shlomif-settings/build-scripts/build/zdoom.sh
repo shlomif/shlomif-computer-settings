646ad60715b1090cc960605d46daf0344501e5b0
Shlomi Fish <shlomif@shlomifish.org>
2015-04-11 18:33:23 +0300

    Update zdoom.

--- shlomif-settings/build-scripts/build/zdoom.sh
+++ shlomif-settings/build-scripts/build/zdoom.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+FMODFOLDER="fmodapi44451linux"
 if [ "$(uname -m)" = "x86_64" ]; then
-    FMODFOLDER="fmodapi42636linux64"
-    FMODFILE="libfmodex64-4.26.36"
+    FMODFILE="libfmodex64-4.44.51"
 else
-    FMODFOLDER="fmodapi42636linux"
-    FMODFILE="libfmodex-4.26.36"
+    FMODFILE="libfmodex-4.44.51"
 fi
 b="$(cd .. && pwd)"
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/apps/zdoom -DFMOD_LIBRARY="$b/$FMODFOLDER/api/lib/$FMODFILE.so" -DFMOD_INCLUDE_DIR="$b/$FMODFOLDER/api/inc" ..
