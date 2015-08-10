9a6996388f42d7eb69ff70eb649d7da9473b3910
Shlomi Fish <shlomif@shlomifish.org>
2015-08-10 14:30:07 +0300

    Remove trailing whitespace

--- shlomif-settings/urpmi-cfg/gen-isoc-i586.pl
+++ shlomif-settings/urpmi-cfg/gen-isoc-i586.pl
@@ -8,7 +8,7 @@ use Template;
 my $template = Template->new();
 
 $template->process(
-    "urpmi.cfg.tt2", 
+    "urpmi.cfg.tt2",
     {
         arch => "i586",
         mdv_mirror => "http://mirror.isoc.org.il/pub/mandriva",
