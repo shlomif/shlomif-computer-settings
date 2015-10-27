8b03d3f1d754bf207239eab213281107d369f12b
Shlomi Fish <shlomif@shlomifish.org>
2015-10-27 20:35:03 +0200

    Add GTK3_EXPERIMENTAL to inkscape-cmake.

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -3,4 +3,6 @@ build_type=''
 if test "$DEBUG" = 1 ; then
     build_type='-DCMAKE_BUILD_TYPE=Debug'
 fi
-cmake -G Ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON ../inkscape/
+flags='-DWITH_GTK3_EXPERIMENTAL=ON'
+
+cmake -G Ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON $flags ../inkscape/
