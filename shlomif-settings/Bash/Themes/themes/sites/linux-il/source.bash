0811fa15a005f0dd5715e89e2aa5cf39ff420d66
Shlomi Fish <shlomif@shlomifish.org>
2016-02-23 21:39:41 +0200

    Add the GitHub repo

--- shlomif-settings/Bash/Themes/themes/sites/linux-il/source.bash
+++ shlomif-settings/Bash/Themes/themes/sites/linux-il/source.bash
@@ -2,12 +2,15 @@ load_common mymake
 load_common prompt
 load_common completion
 load_common no_implicit_make
+load_common git
 
-base="$HOME/Docs/IGLU/linux.org.il/linux.org.il-new-site"
+base="$HOME/Docs/IGLU/linux.org.il/linux.org.il"
 repo="$base"
 trunk="$repo"
 src="$trunk/www.linux.org.il-new-site/template-toolkit"
 this="$src"
+
+remote_repo="$($__themes_dir/common/github-git-remote-repo.pl --user="Hamakor" --repo="linux.org.il")"
 
 cd $this
 
