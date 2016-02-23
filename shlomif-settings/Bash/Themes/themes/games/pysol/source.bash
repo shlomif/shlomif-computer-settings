dd22c11c5e553b9bca69a0d69ebe42536b4e6256
Shlomi Fish <shlomif@shlomifish.org>
2016-02-23 21:42:24 +0200

    add a theme for working on PySol FC.

---
+++ shlomif-settings/Bash/Themes/themes/games/pysol/source.bash
@@ -0,0 +1,47 @@
+load_common mymake
+load_common completion
+load_common prompt
+load_common git
+
+base="$HOME/Download/unpack/games/pysolfc"
+trunk="$base/git/PySolFC"
+proj="$trunk"
+a="$proj"
+
+this="$trunk"
+
+remote_repo="$($__themes_dir/common/github-git-remote-repo.pl --user="shlomif" --repo="PySolFC")"
+
+export FCS_USE_TEST_RUN=1
+
+cd "$this"
+
+t()
+{
+    cd "$this" && make test
+}
+
+prompt()
+{
+    __prompt_cmd \
+        "\$trunk=$trunk" \
+        "\$base=$base" \
+        "~=$HOME"
+}
+
+L()
+{
+    (cd "$this/pysollib/$1" && ls *.py)
+}
+
+dups()
+{
+    for i in $(L tk) ; do
+        if cmp -s "$this/pysollib/tk/$i"  "$this/pysollib/tile/$i" ; then
+            echo "$i"
+        fi
+    done
+}
+
+proj_name='PySolFC'
+
