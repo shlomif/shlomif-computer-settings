31bc20b5d8475f79cd391354399d8df51240a0d3
Shlomi Fish <shlomif@shlomifish.org>
2016-04-02 16:09:05 +0300

    dos2unix+trailing space

--- shlomif-settings/home-bin-executables/bin/find-leaf-rpms.pl
+++ shlomif-settings/home-bin-executables/bin/find-leaf-rpms.pl
@@ -2,14 +2,23 @@
 
 use strict;
 use warnings;
+use autodie;
+my %installed = (map { chomp; $_ => +{} } `rpm -qa --qf "%{n}\\n" | grep -vP '^(task-shlomif|msttcorefonts)\$'`);
 
-my %installed = (map { chomp; $_ => 1 } `rpm -qa --qf "%{n}\\n"`);
-
-foreach my $rpm (sort keys %installed)
+my @installed_rpms = sort keys %installed;
+open my $in_fh, qq#urpmq --debug --whatrequires @installed_rpms 2>&1 |#;
+while (my $l = <$in_fh>)
 {
-    my @deps = `urpmq --whatrequires "$rpm"`;
-    chomp @deps;
-    my %l = ($rpm => 1);
-    my @found= sort grep { exists $installed{$_} and !$l{$_}++} @deps;
+    if (my ($requires, $what_req) = ($l =~ /\Aadding package (\S+) \(requires ([^\) ]+)/))
+    {
+        if (exists $installed{$requires} and exists $installed{$what_req})
+        {
+            $installed{$what_req}{$requires} = 1;
+        }
+    }
+}
+foreach my $rpm (@installed_rpms)
+{
+    my @found = grep { $_ ne $rpm } sort keys %{$installed{$rpm}};
     print "$rpm\t" . scalar(@found) . "\t" . join(',', @found) . "\n";
 }
