53db7a536be4b8f96b92c5b8af7567bf3214f3c0
Shlomi Fish <shlomif@shlomifish.org>
2016-04-08 12:41:18 +0300

    add more files to the history backup.

--- shlomif-settings/Bash/history-backup/backup-bash-history.pl
+++ shlomif-settings/Bash/history-backup/backup-bash-history.pl
@@ -11,23 +11,48 @@ use POSIX (qw(strftime));
 
 my $backup_base = "$ENV{HOME}/Backup/bash-history";
 my $trunk = "$backup_base/git-repos";
-copy("$ENV{HOME}/.bash_history", "$trunk/bash-history/bash_history");
-my $date=strftime("+%Y-%m-%d-%H:%M:%S", localtime());
+my $files_dir = 'bash-history';
+my $trunk_files = "$trunk/$files_dir";
 
-my $file = shell_quote("$trunk/bash-history/bash_history");
-my $num_lines=`wc -l $file`;
-($num_lines) = ($num_lines =~ m{(\d+)});
+my $H = $ENV{HOME};
+my @files =
+(
+    {
+        from => "$H/.bash_history",
+        to => "bash_history",
+    },
+    {
+        from => "$H/.histfile",
+        to => "histfile",
+    },
+    {
+        from => "$H/.mrconfig",
+        to => "mrconfig",
+    },
+);
+
+foreach my $f (@files)
+{
+    my $to = "$trunk_files/$f->{to}";
+    copy($f->{from}, $to);
+    my $fn = shell_quote($to);
+    my ($num_lines) = (`wc -l $fn` =~ m{([0-9]+)})
+        or die "Cannot find num lines for $fn.";
+    $f->{num_lines} = $num_lines;
+}
+
+my $date=strftime("+%Y-%m-%d-%H:%M:%S", localtime());
 
 chdir($trunk) or die "Cannot chdir to '$trunk'";
 
-if (system("git" , "add", "bash-history/bash_history"))
+if (system("git" , "add", map { "$files_dir/$_->{to}" } @files))
 {
-    die "git command 1 failed.";
+    die "git command add failed.";
 }
 
 if (`git status -s .` =~ /\S/)
 {
-    if (system("git", "commit", "-q", "-m", "Commiting the bash_history as of $date\n\nContaining $num_lines lines."))
+    if (system("git", "commit", "-q", "-m", "Commiting the bash_history as of $date\n\n" . join("\n", map { "$_->{to} containing $_->{num_lines} lines." } @files)))
     {
         die "git commit command failed.";
     }
