56d3620f81ccc0e1c7b6e5c14b102ce95e151cb1
Shlomi Fish <shlomif@shlomifish.org>
2016-04-12 16:39:04 +0300

    Do not install some no longer needed modules

--- shlomif-settings/home-bin-executables/bin/perl-Configure-CPANPLUS-DWIM.sh
+++ shlomif-settings/home-bin-executables/bin/perl-Configure-CPANPLUS-DWIM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PERL_AUTOINSTALL=--alldeps
 perl -MCPANPLUS::Configure -e 'my $conf = CPANPLUS::Configure->new(); $conf->set_conf("prereqs" => 1); $conf->save();'
-cpanp -i Term::ReadLine::Gnu KiokuDB KiokuDB::Backend::DBI DBD::SQLite Moose
+# cpanp -i Term::ReadLine::Gnu KiokuDB KiokuDB::Backend::DBI DBD::SQLite Moose
+cpanp -i Term::ReadLine::Gnu
