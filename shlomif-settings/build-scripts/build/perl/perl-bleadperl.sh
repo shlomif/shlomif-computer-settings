bb1e0a723118ba8f0d69c115a96b6a6ee1a68f96
Shlomi Fish <shlomif@shlomifish.org>
2016-04-15 20:41:03 +0300

    add some warning flags.

--- shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
+++ shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f config.sh Policy.sh
 # -flto does not work properly in gcc-4.9.x without -ffat-lto-objects
-sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Doptimize='-O3 -march=native' -Dusedevel
+sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Dccflags='-Wall -Wextra -Wformat -Werror=format-security' -Doptimize='-O3 -march=native' -Dusedevel
