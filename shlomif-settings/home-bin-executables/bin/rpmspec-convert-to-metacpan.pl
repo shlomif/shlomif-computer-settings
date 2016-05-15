c555c1cc85f1c6ae80f459fde906cb6ea1348d05
Shlomi Fish <shlomif@shlomifish.org>
2016-05-15 16:27:24 +0300

    add modernisations

--- shlomif-settings/home-bin-executables/bin/rpmspec-convert-to-metacpan.pl
+++ shlomif-settings/home-bin-executables/bin/rpmspec-convert-to-metacpan.pl
@@ -10,5 +10,7 @@ while (<>)
     {
         s#http://search.cpan.org/dist/#http://metacpan.org/release/#;
     }
+    s#\A%makeinstall_std *\z#%make_install#;
+    s#\A%make\b#%make_build#;
     print "$_\n";
 }
