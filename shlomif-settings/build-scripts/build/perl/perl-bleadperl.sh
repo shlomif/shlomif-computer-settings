033b70347a9b35bd75957a23c0b885b592929a8e
Shlomi Fish <shlomif@shlomifish.org>
2016-06-19 22:45:00 +0300

    Add the man1dir=none man3dir=none thing
    to speed up the build.

--- shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
+++ shlomif-settings/build-scripts/build/perl/perl-bleadperl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -f config.sh Policy.sh
 # -flto does not work properly in gcc-4.9.x without -ffat-lto-objects
-sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Dccflags='-Wall -Wextra -Wformat -Werror=format-security' -Doptimize='-O3 -march=native -flto -ffat-lto-objects' -Dusedevel
+sh Configure -de -Dprefix=$HOME/apps/perl/bleadperl -Dccflags='-Wall -Wextra -Wformat -Werror=format-security' -Doptimize='-O3 -march=native -flto -ffat-lto-objects' -Dusedevel \
+    -Dman1dir=none -Dman3dir=none
