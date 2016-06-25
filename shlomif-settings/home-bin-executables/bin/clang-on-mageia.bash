830066e702e80f6ed71351d9f7de7d4a950ff143
Shlomi Fish <shlomif@shlomifish.org>
2016-06-25 15:19:47 +0300

    Add the clang-on-Mageia configuration script.

---
+++ shlomif-settings/home-bin-executables/bin/clang-on-mageia.bash
@@ -0,0 +1,5 @@
+gcc_ver=5.4.0
+export COMPILER_PATH=/usr/lib/gcc/x86_64-mageia-linux-gnu/"$gcc_ver"
+export CPATH=/usr/include/c++/"$gcc_ver"/:/usr/include/c++/"$gcc_ver"/x86_64-mageia-linux-gnu
+export LIBRARY_PATH="$COMPILER_PATH"
+export LD=/usr/bin/ld.gold
