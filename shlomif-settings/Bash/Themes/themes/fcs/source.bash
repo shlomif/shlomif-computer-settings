1f4e42b5da60fbd955775637a42623c9dc978346
Shlomi Fish <shlomif@shlomifish.org>
2016-07-03 13:38:54 +0300

    Add an env var for clang-format.

--- shlomif-settings/Bash/Themes/themes/fcs/source.bash
+++ shlomif-settings/Bash/Themes/themes/fcs/source.bash
@@ -49,7 +49,7 @@ setup()
 
 export LIBAVL2_SOURCE_DIR="$HOME/Download/unpack/prog/c/avl-2.0.3/"
 export FCS_USE_TEST_RUN=1
-export FCS_PGO_THEME="-l ct"
+export FCS_PGO_THEME="-l ct" FCS_TEST_CLANG_FORMAT=1
 
 # This causes problems with the threaded solver.
 unset MALLOC_CHECK_
