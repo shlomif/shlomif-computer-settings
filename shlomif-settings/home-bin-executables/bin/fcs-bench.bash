70d40582e6d251fb30aa472126fa49173680cb56
Shlomi Fish <shlomif@shlomifish.org>
2016-08-16 13:03:08 +0300

    Run both forking and multithreaded

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -3,9 +3,11 @@
 repeat_count="$1"
 shift
 
+export LD_LIBRARY_PATH=/home/shlomif/Download/unpack/os-zero/OS-Zero/usr/lib/c/test
+
 num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"
 
 for I in $(seq 1 "$repeat_count") ; do
-    # ARGS="--worker-step 16 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
+    ARGS="--worker-step 16 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
     ARGS="--worker-step 25 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash -p "$num_cpus" "$num_cpus"
 done
