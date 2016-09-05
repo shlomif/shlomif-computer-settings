9c6aef93d2015b4c0e2e84d81fbfb893cd1a0aac
Shlomi Fish <shlomif@shlomifish.org>
2016-09-06 00:18:44 +0300

    tweak the gnumeric script

--- shlomif-settings/build-scripts/build/gnumeric.sh
+++ shlomif-settings/build-scripts/build/gnumeric.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PKG_CONFIG_PATH="$HOME/apps/gnumeric/lib/pkgconfig"
-./configure --prefix="$HOME/apps/gnumeric" --disable-component
+CFLAGS="-g" ./configure --prefix="$HOME/apps/gnumeric" --disable-component
