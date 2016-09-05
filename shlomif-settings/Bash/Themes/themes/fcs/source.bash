f274612394f21891b73b9de030aaed9c4f30cff1
Shlomi Fish <shlomif@shlomifish.org>
2016-09-06 00:43:33 +0300

    Convert to _github_shlomif.

--- shlomif-settings/Bash/Themes/themes/fcs/source.bash
+++ shlomif-settings/Bash/Themes/themes/fcs/source.bash
@@ -2,12 +2,14 @@ load_common mymake
 load_common completion
 load_common prompt
 load_common no_implicit_make
+load_common git
 
+remote_repo="$(_shlomif_github "fc-solve")"
 fcs="$HOME/progs/freecell"
 base="$fcs"
 branches="$fcs/branches"
-_base_trunk="$fcs/git"
-trunk="$_base_trunk/fc-solve"
+git_base="$fcs/git"
+trunk="$git_base/fc-solve"
 t_fcs="$trunk/fc-solve"
 c_src="$t_fcs/source"
 c="$c_src"
@@ -34,18 +36,6 @@ pats="$hg_pats/patsolve"
 
 this="$c_src"
 # this="$dd_branch"
-
-setup()
-{
-    (
-        mkdir -p "$_base_trunk"
-        cd "$_base_trunk"
-        git clone 'ssh://git@bitbucket.org/shlomif/fc-solve.git' "$trunk"
-        cd "$trunk"
-        git remote add github 'git@github.com:shlomif/fc-solve.git'
-    )
-    cd "$this"
-}
 
 export LIBAVL2_SOURCE_DIR="$HOME/Download/unpack/prog/c/avl-2.0.3/"
 export FCS_USE_TEST_RUN=1
