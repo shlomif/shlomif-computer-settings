eef6a9cc0a9628308409a5fcd4c89ecb400c8756
Shlomi Fish <shlomif@shlomifish.org>
2016-09-06 00:19:46 +0300

    Make -p and -t grouped instead of interleaved.

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -7,7 +7,9 @@ export LD_LIBRARY_PATH=/home/shlomif/Download/unpack/os-zero/OS-Zero/usr/lib/c/t
 
 num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"
 
-for I in $(seq 1 "$repeat_count") ; do
+for i in $(seq 1 "$repeat_count") ; do
     ARGS="--worker-step 16 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
+done
+for i in $(seq 1 "$repeat_count") ; do
     ARGS="--worker-step 25 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash -p "$num_cpus" "$num_cpus"
 done
