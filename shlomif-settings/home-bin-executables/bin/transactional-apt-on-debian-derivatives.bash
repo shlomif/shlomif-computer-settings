a357d0afa0ec4192bb0138b8e71cf4d29d9473ef
Shlomi Fish <shlomif@shlomifish.org>
2016-09-24 19:32:13 +0300

    Add the transactional apt program.

---
+++ shlomif-settings/home-bin-executables/bin/transactional-apt-on-debian-derivatives.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright by Shlomi Fish, 2016 under the
+# https://opensource.org/licenses/mit-license.php
+
+# This program installs a transaction of apt updates on Debian and derivatives
+# (Ubuntu, Mint, etc.). It may not be the recommended way to do it so:
+# USE AT YOUR OWN RISK.
+
+apt-get -y --no-install-recommends install $(apt list --upgradable | grep -v task | tail -50 | perl -lapE 's#/.*##')
