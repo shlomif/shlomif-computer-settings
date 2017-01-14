a78603b900aadd6b0b9e2a8dde940bbbefc74960
Shlomi Fish <shlomif@shlomifish.org>
2017-01-14 14:22:06 +0200

    Add more to gimp-git-all-deps.bash

--- shlomif-settings/build-scripts/build/gimp-git-all-deps.bash
+++ shlomif-settings/build-scripts/build/gimp-git-all-deps.bash
@@ -1,14 +1,29 @@
 #!/bin/bash
+set -x
 gegl_p="$HOME/apps/graphics/gegl"
 babl_p="$HOME/apps/graphics/babl"
+mypaint_p="$HOME/apps/graphics/libmypaint"
 export PATH="$gegl_p/bin:$PATH"
-export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$HOME/apps/graphics/libmypaint/lib/pkgconfig:$babl_p/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$mypaint_p/lib/pkgconfig:$babl_p/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-babl_git_checkout="/home/shlomif/Download/unpack/graphics/gimp/babl/git/babl"
-if ! test -e "$babl_git_checkout" ; then
-    mkdir -p "$(dirname "$babl_git_checkout")"
-    git clone git://git.gnome.org/babl "$babl_git_checkout"
-fi
-( cd "$babl_git_checkout" && NOCONFIGURE=1 ./autogen.sh && ./configure --prefix="$babl_p" && make && make check && make install ) || { echo failed ; exit -1 ; }
-CFLAGS="-g" \
-    ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode
+autoconf_git_build()
+{
+    local git_co="$1"
+    shift
+    local url="$1"
+    shift
+    local prefix="$1"
+    shift
+
+    if ! test -e "$git_co" ; then
+        mkdir -p "$(dirname "$git_co")"
+        git clone "$url" "$git_co"
+    fi
+    ( cd "$git_co" && NOCONFIGURE=1 ./autogen.sh && ./configure --prefix="$prefix" && make && make check && make install ) || { echo failed ; exit -1 ; }
+}
+
+autoconf_git_build "/home/shlomif/Download/unpack/graphics/gimp/babl/git/babl" git://git.gnome.org/babl "$babl_p"
+autoconf_git_build "/home/shlomif/Download/unpack/graphics/gimp/gegl/git/gegl" git://git.gnome.org/babl "$gegl_p"
+autoconf_git_build "/home/shlomif/Download/unpack/graphics/gimp/libmypaint/git/libmypaint" https://github.com/mypaint/libmypaint.git "$mypaint_p"
+
+# CFLAGS="-g"  ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode
