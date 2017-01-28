54700fb1e2fcd77244eeb063067dc586012552fd
Shlomi Fish <shlomif@shlomifish.org>
2017-01-29 00:38:18 +0200

    add dedup from stack exchange.

---
+++ shlomif-settings/Bash/bashrc/bashrc.d/dedup.bash
@@ -0,0 +1,14 @@
+# Taken from http://unix.stackexchange.com/questions/40749 - thanks!
+# Deduplicate path variables
+get_var () {
+    eval 'printf "%s" "${'"$1"'}"'
+}
+set_var () {
+    eval "$1=\"\$2\""
+}
+dedup_pathvar () {
+    pathvar_name="$1"
+    pathvar_value="$(get_var "$pathvar_name")"
+    deduped_path="$(perl -e 'print join(":",grep { not $seen{$_}++ } split(/:/, $ARGV[0]))' "$pathvar_value")"
+    set_var "$pathvar_name" "$deduped_path"
+}
