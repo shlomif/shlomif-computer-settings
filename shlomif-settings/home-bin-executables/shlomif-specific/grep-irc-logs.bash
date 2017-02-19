24658225eeb78ab17e9a6cc29fedfda64d3b106e
Shlomi Fish <shlomif@shlomifish.org>
2017-02-19 20:59:07 +0200

    A script to grep my IRC logs.
    It's just a start but it finds stuff in several past and present
    locations.

---
+++ shlomif-settings/home-bin-executables/shlomif-specific/grep-irc-logs.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chan_pat="$1"
+shift
+
+ls ~/Backup/old-dot-files/dot-xchat2/xchatlogs/*.log \
+   ~/Backup/old-dot-files/dot-xchat2/__keep-xchatlogs/*.log \
+   ~/Backup/old-dot-files/dot-xchat2/saved-xchatlogs/*.log \
+   ~/.config/hexchat/logs/*.log \
+   ~/Backup/laptop/2017-01-04/2017-01-04/disk-fs/home/shlomif/.config/hexchat/logs/*/*.log \
+   ~/Backup/laptop/2012-12-22/disk-fs/extra-data/unpack-for-grep/.xchat2/xchatlogs/*.log \
+   | grep -iP "$chan_pat"
