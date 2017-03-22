0a9eb5961a86c68b601cadb9e517fa454ad07b77
Shlomi Fish <shlomif@shlomifish.org>
2017-03-22 02:15:39 +0200

    Remove cruft and optimise to avoid seq

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -3,13 +3,25 @@
 repeat_count="$1"
 shift
 
-export LD_LIBRARY_PATH=/home/shlomif/Download/unpack/os-zero/OS-Zero/usr/lib/c/test
-
 num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"
 
-for i in $(seq 1 "$repeat_count") ; do
+loop()
+{
+    let i=0
+    while test "$i" -lt "$repeat_count" ; do
+        f
+        let i++
+    done
+}
+
+f()
+{
     ARGS="--worker-step 16 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
-done
-for i in $(seq 1 "$repeat_count") ; do
+}
+loop
+
+f()
+{
     ARGS="--worker-step 25 $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash -p "$num_cpus" "$num_cpus"
-done
+}
+loop
