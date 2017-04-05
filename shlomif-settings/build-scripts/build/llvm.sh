0c087fa7880b1a6e90c02a31874c0c1ce17cc680
Shlomi Fish <shlomif@shlomifish.org>
2017-04-05 14:20:43 +0300

    add lto support

--- shlomif-settings/build-scripts/build/llvm.sh
+++ shlomif-settings/build-scripts/build/llvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 b='/opt/llvm'
+export LD='/usr/bin/ld.gold'
 export PATH="$b/bin:$PATH"
 export PKG_CONFIG_PATH="$b/lib64/pkgconfig:$b/lib/pkgconfig:$PKG_CONFIG_PATH"
-cmake -DCMAKE_BUILD_TYPE="release" -DCMAKE_INSTALL_PREFIX="$b" "$@"
+cmake -DCMAKE_BUILD_TYPE="release" -DCMAKE_INSTALL_PREFIX="$b" -DLLVM_BINUTILS_INCDIR=/usr/lib/gcc/x86_64-mageia-linux-gnu/5.4.0/plugin/include/ "$@"
