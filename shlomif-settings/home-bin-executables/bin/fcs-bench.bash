de0dd950fccdd03c2754862c55f7eb35a7fbea83
Shlomi Fish <shlomif@shlomifish.org>
2017-09-18 12:21:13 +0300

    Correct path.

--- shlomif-settings/home-bin-executables/bin/fcs-bench.bash
+++ shlomif-settings/home-bin-executables/bin/fcs-bench.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # export LD_PRELOAD=/home/shlomif/Download/unpack/prog/lockless_allocator/libllalloc.so.1.3
+# export LD_PRELOAD=/home/shlomif/Download/unpack/prog/c/ltalloc/gnu.make.lib/libltalloc.so
 export LD_LIBRARY_PATH=/home/shlomif/apps/tcmalloc/lib
 
 repeat_count="$1"
@@ -22,7 +23,7 @@ flags=''
 
 f()
 {
-    ARGS="--worker-step $step $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash $flags "$num_cpus" "$num_cpus"
+    ARGS="--worker-step $step $FCS_PGO_THEME" bash "${SRC_DIR:-.}"/../scripts/time-threads-num.bash $flags "$num_cpus" "$num_cpus"
 }
 loop
 
