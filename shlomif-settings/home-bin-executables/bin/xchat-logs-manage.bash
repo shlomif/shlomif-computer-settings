42926ab5da32c1b41bb2825cb37a35e9be2b59e2
Shlomi Fish <shlomif@shlomifish.org>
2017-09-18 12:22:41 +0300

    add an alias

--- shlomif-settings/home-bin-executables/bin/xchat-logs-manage.bash
+++ shlomif-settings/home-bin-executables/bin/xchat-logs-manage.bash
@@ -1,5 +1,6 @@
 # This code should be sourced into the current shell using
 # Bash's "." or "source".
-unalias c d
+unalias c d k
 c() { f="$(ls -S | tail -1)"; echo "== $f == " ; cat "$f"; }
 d() { rm -f "$f"; unset f; }
+k() { mv "$f" ../__keep-xchatlogs/ ; unset f; }
