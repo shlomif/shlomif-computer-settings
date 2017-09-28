be069cb6296cbf6884d1ef4fa2cadb9cee319b82
Shlomi Fish <shlomif@shlomifish.org>
2017-09-28 19:31:01 +0300

    Enhance more.

--- shlomif-settings/Bash/Themes/themes/perl/begin/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/begin/source.bash
@@ -1,3 +1,4 @@
+load_common git
 load_common mymake
 load_common prompt
 load_common completion
@@ -6,6 +7,7 @@ load_common no_implicit_make
 base="$HOME/Docs/programming/Perl/web-sites/perl-begin"
 trunk="$base/trunk"
 this="$trunk"
+remote_repo="$(_shlomif_github "perl-begin")"
 
 cd $this
 
