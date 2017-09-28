93b9c68dfd72feeaff07a3a2a9cdf76c6eafaade
Shlomi Fish <shlomif@shlomifish.org>
2017-09-28 19:30:02 +0300

    path in laptop

--- shlomif-settings/home-bin-executables/shlomif-specific/grep-irc-logs.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/grep-irc-logs.bash
@@ -7,6 +7,7 @@ ls ~/Backup/old-dot-files/dot-xchat2/xchatlogs/*.log \
    ~/Backup/old-dot-files/dot-xchat2/__keep-xchatlogs/*.log \
    ~/Backup/old-dot-files/dot-xchat2/saved-xchatlogs/*.log \
    ~/.config/hexchat/logs/*.log \
+   ~/.config/hexchat/logs/*/*.log \
    ~/Backup/laptop/2017-04-04/disk-fs/home/shlomif/.config/hexchat/logs/*/*.log \
    ~/Backup/laptop/2012-12-22/disk-fs/extra-data/unpack-for-grep/.xchat2/xchatlogs/*.log \
    | grep -iP "$chan_pat"
