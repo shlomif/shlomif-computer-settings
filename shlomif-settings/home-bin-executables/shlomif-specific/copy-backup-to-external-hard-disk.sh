bf5a91fee27f3930edda0590ca82c5634f3ec548
Shlomi Fish <shlomif@shlomifish.org>
2017-10-27 17:37:09 +0300

    update

--- shlomif-settings/home-bin-executables/shlomif-specific/copy-backup-to-external-hard-disk.sh
+++ shlomif-settings/home-bin-executables/shlomif-specific/copy-backup-to-external-hard-disk.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # cp -rL --preserve "$1" /mnt/external/shlomi/Data-Backup/
-cp -rL --preserve "$1" /media/My\ Passport/Shlomif/Backups/Data/telaviv1.shlomifish.org/
+# cp -rL --preserve "$1" /media/My\ Passport/Shlomif/Backups/Data/telaviv1.shlomifish.org/
+cp -rL --preserve "$1" /run/media/shlomif/My\ Passport/Shlomif/Backups/Data/telaviv1.shlomifish.org/
