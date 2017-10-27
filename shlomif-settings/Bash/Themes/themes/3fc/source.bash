08c0027e86c0fc9938a55efdd3bc88e89dc0dc24
Shlomi Fish <shlomif@shlomifish.org>
2017-10-27 18:42:08 +0300

    Add the 3fc 3 freecells stats theme.

---
+++ shlomif-settings/Bash/Themes/themes/3fc/source.bash
@@ -0,0 +1,51 @@
+load_common mymake
+load_common completion
+load_common prompt
+load_common no_implicit_make
+load_common git
+
+remote_repo="$(_shlomif_github "freecell-3fc-determine-solvability")"
+git_base="$HOME"/Download/unpack/games/freecell/freecell-3fc-determine-solvability
+this="$git_base"
+c="$this"
+
+cd "$this"
+
+c()
+{
+    cd "$c"
+}
+
+export PATH="$PATH:$HOME/Download/unpack/prog/python/pypy2-v5.7.1-src/pypy/goal/"
+
+t()
+{
+    (
+        set -x
+        cd "$this" && \
+        cp -f ~/Backup/Arcs/fc-solve/from-amazon-aws/downloaded/aws-2-3fc-output.txt . && \
+        pypy-c summarize1.py && \
+        git au . && \
+        git commit -m "Add more from amazon.com AWS - thanks." && \
+        git push
+    )
+}
+
+cb()
+{
+    cd "$b"
+}
+
+i()
+{
+    cd "$i"
+}
+
+prompt()
+{
+    __prompt_cmd \
+        "\$this=$this" \
+        "~=$HOME"
+}
+
+proj_name='3fc'
