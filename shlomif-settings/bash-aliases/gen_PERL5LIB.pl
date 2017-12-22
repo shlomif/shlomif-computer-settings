11b8450979777962109950d8f50fca67b6d9f3af
Shlomi Fish <shlomif@shlomifish.org>
2017-12-22 21:36:26 +0200

    update.

--- shlomif-settings/bash-aliases/gen_PERL5LIB.pl
+++ shlomif-settings/bash-aliases/gen_PERL5LIB.pl
@@ -5,7 +5,7 @@ use warnings;
 
 my @d;
 # foreach my $ver (qw( 5.22.2 5.22.1 5.22.0 5.20.1 5.20.0 5.18.2 5.18.1 ))
-foreach my $ver (qw( 5.26.0 5.22.3 5.22.2 ))
+foreach my $ver (qw( 5.26.1 5.22.3 5.22.2 ))
 {
     foreach my $dir (qw(lib/perl5/site_perl lib/site_perl lib/perl5))
     {
