3a9d3cfea869605fa79158b2250cd80e645c6b86
Shlomi Fish <shlomif@shlomifish.org>
2018-01-02 19:10:44 +0200

    update the gimp git build prog

--- shlomif-settings/build-scripts/build/gimp-git-all-deps.bash
+++ shlomif-settings/build-scripts/build/gimp-git-all-deps.bash
@@ -4,7 +4,7 @@ gegl_p="$HOME/apps/graphics/gegl"
 babl_p="$HOME/apps/graphics/babl"
 mypaint_p="$HOME/apps/graphics/libmypaint"
 export PATH="$gegl_p/bin:$PATH"
-export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$mypaint_p/lib/pkgconfig:$babl_p/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$mypaint_p/lib/pkgconfig:$mypaint_p/share/pkgconfig:$babl_p/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 _check()
 {
@@ -23,17 +23,20 @@ autoconf_git_build()
     shift
     local prefix="$1"
     shift
+    local branch="${1:-master}"
+    shift
 
     if ! test -e "$git_co" ; then
         mkdir -p "$(dirname "$git_co")"
         git clone "$url" "$git_co"
     fi
-    ( cd "$git_co" && git s origin && NOCONFIGURE=1 ./autogen.sh && ./configure --prefix="$prefix" && make && _check && make install ) || { echo failed ; exit -1 ; }
+    ( cd "$git_co" && git checkout "$branch" && git s origin "$branch" &&  NOCONFIGURE=1 ./autogen.sh && ./configure --prefix="$prefix" && make && _check && make install ) || { echo failed ; exit -1 ; }
 }
 
 autoconf_git_build "$HOME/Download/unpack/graphics/gimp/babl/git/babl" git://git.gnome.org/babl "$babl_p"
 autoconf_git_build "$HOME/Download/unpack/graphics/gimp/gegl/git/gegl" git://git.gnome.org/gegl "$gegl_p"
 autoconf_git_build "$HOME/Download/unpack/graphics/gimp/libmypaint/git/libmypaint" https://github.com/mypaint/libmypaint.git "$mypaint_p"
+autoconf_git_build "$HOME/Download/unpack/graphics/gimp/libmypaint/git/mypaint-brushes" https://github.com/Jehan/mypaint-brushes.git "$mypaint_p" "v1.3.x"
 autoconf_git_build "$HOME/Download/unpack/graphics/gimp/git/gimp" https://git.gnome.org/git "$HOME/apps/gimp-devel"
 
 # CFLAGS="-g"  ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode
