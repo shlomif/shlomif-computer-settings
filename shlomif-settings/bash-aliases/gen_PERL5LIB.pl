6c2fd0b82845879f84b8f99b45b6f408d339d089
Shlomi Fish <shlomif@shlomifish.org>
2018-01-09 22:17:54 +0200

    remove old vers.

--- shlomif-settings/bash-aliases/gen_PERL5LIB.pl
+++ shlomif-settings/bash-aliases/gen_PERL5LIB.pl
@@ -4,13 +4,14 @@ use strict;
 use warnings;
 
 my @d;
+
 # foreach my $ver (qw( 5.22.2 5.22.1 5.22.0 5.20.1 5.20.0 5.18.2 5.18.1 ))
-foreach my $ver (qw( 5.26.1 5.22.3 5.22.2 ))
+foreach my $ver (qw( 5.26.1 ))
 {
     foreach my $dir (qw(lib/perl5/site_perl lib/site_perl lib/perl5))
     {
-        push @d, "$ENV{HOME}/apps/perl/modules/$dir/$ver"
+        push @d, "$ENV{HOME}/apps/perl/modules/$dir/$ver";
     }
 }
 
-print "export PERL5LIB='" . join(":",@d) . "'\n";
+print "export PERL5LIB='" . join( ":", @d ) . "'\n";
