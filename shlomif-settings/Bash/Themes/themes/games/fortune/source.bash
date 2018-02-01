42bdbda4061728eb2bbc36a21e90255aab598b22
Shlomi Fish <shlomif@shlomifish.org>
2018-02-01 17:15:59 +0200

    Cleanups and enhancements.

--- shlomif-settings/Bash/Themes/themes/games/fortune/source.bash
+++ shlomif-settings/Bash/Themes/themes/games/fortune/source.bash
@@ -9,16 +9,11 @@ base="$HOME/progs/C/fortune-mod"
 git_base="$base/git"
 trunk="$git_base/fortune-mod"
 this="$trunk"
-# this="$dd_branch"
+build="$git_base/B"
 
-export LIBAVL2_SOURCE_DIR="$HOME/Download/unpack/prog/c/avl-2.0.3/"
 export FCS_USE_TEST_RUN=1
-export FCS_PGO_THEME="-l obf" FCS_TEST_CLANG_FORMAT=1
 
-# This causes problems with the threaded solver.
-unset MALLOC_CHECK_
-
-cd $this
+cd "$this"
 
 prompt()
 {
@@ -43,7 +38,12 @@ t()
 {
     (
         export HARNESS_BREAK=1
-        cd "$git_base/B" && \
+        if ! test -e "$build"
+        then
+            mkdir "$build"
+        fi
+        cd "$build" && \
+            cmake "$trunk" && \
             make && \
             perl "$trunk"/run-tests.pl --src-dir="$trunk"
         n --msg "Freecell Solver Test Finished"
@@ -99,45 +99,4 @@ pb()
     cd "$pats_b"
 }
 
-# Short for format.
-fmt()
-{
-    (
-        cd "$t_fcs/B" && \
-            make -j4 && \
-            perl "$c_src"/run-tests.pl --glob='{clang-format,perltidy,style-trailing-space}*.t'
-    )
-}
-
-bpat()
-{
-    (
-    mkdir -p "$pats_b"
-    pb
-    cmake "$pats" && make -j4 && make check && rm -fr "$pats_b" && rm -f "$pats/cmake"/*.cmake
-    )
-}
-
-bp()
-{
-    bpat
-}
-
-ti1()
-{
-    perl "$c_src"/scripts/time-fcs.pl DUMPS-*/*
-}
-
-ti()
-{
-     ti1 | tac | cat -n | perl -lapE 's/:/\t/g; ' | less
-}
-
-proj_name='fcs'
-
-# Clean up the environment for the valgrind tests to succeed.
-# Commented out because now it makes things worse in t/t/build-process.t
-# file.
-# unset WD BROWSER CANBERRA_DRIVER CMAKE_MODULE_PATH DESKTOP_SESSION EDITOR G_FILENAME_ENCODING G_SLICE HARNESS_SUMMARY_COL_FAIL HARNESS_SUMMARY_COL_SUC HGEDITOR H HOMEPAGE_SSH_PATH LADSPA_PATH LAN LANGUAGE LC_ADDRESS LC_COLLATE LC_CTYPE LC_IDENTIFICATION LC_MESSAGES LC_MESSAGES LC_MESSAGES LC_NAME LC_NUMERIC LC_PAPER LC_SOURCES LC_TELEPHONE LC_TIME LESSCHARSET MAIL MANPATH MALLOC_PERTURB_ META_CLASS MGA_MENU_STYLE MYVIMRC NLSPATH NOPASTE_SERVICES PERL_AUTOINSTALL PERL_MM_USE_DEFAULT PILOTPORT PYTHONDONTWRITEBYTECODE PYTHONSTARTUP QT4DOCDIR QTDIR QTINC QTLIB QT_XFT SAL_USE_VCLPLUGIN SCREENDIR SVN_EDITOR SYSTEMD_PAGER VIM VIMRUNTIME WD XDG_CURRENT_DESKTOP XDG_MENU_PREFIX _LXSESSION_PID
-
-# export LC_ALL=en_US.UTF-8
+proj_name='fortune'
