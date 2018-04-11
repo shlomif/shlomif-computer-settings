7d159b6e99e6637b65f31d94fa348c4eae6bd554
Shlomi Fish <shlomif@shlomifish.org>
2018-04-11 21:22:11 +0300

    Add a theme for editing the email signature.

---
+++ shlomif-settings/Bash/Themes/themes/signature/source.bash
@@ -0,0 +1,50 @@
+load_common mymake
+load_common prompt
+load_common completion
+load_common no_implicit_make
+load_common git
+
+remote_repo="$(_shlomif_github "shlomif-email-signature")"
+
+base="$HOME/Docs/Personal/email"
+git_base="$base"
+trunk="$base/signature"
+repo="$trunk"
+src="$trunk"
+this="$src"
+
+s="$src"
+c="$s"
+cd "$this"
+
+prompt()
+{
+    __prompt_cmd \
+        "\$src=$src" \
+        "\$trunk=$trunk" \
+        "\$base=$base" \
+        "~=$HOME"
+}
+
+b()
+{
+    (
+        cd "$trunk/b" && \
+        cmake "$src" && \
+        make
+    )
+}
+
+t()
+{
+    (
+        cd "$src" && sh test.sh
+    )
+}
+
+e()
+{
+    cd "$src" && gvim -o gen-shlomif-at-iglu-sig shlomif-sig-quotes.txt
+}
+
+proj_name='EmailSignature'
