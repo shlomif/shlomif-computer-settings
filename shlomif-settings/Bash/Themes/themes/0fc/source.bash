7acd034bc838dfb4bcd0fe14f969a9dacbdb7c9c
Shlomi Fish <shlomif@shlomifish.org>
2018-04-22 18:35:12 +0300

    Add the bash theme.

---
+++ shlomif-settings/Bash/Themes/themes/0fc/source.bash
@@ -0,0 +1,38 @@
+load_common mymake
+load_common completion
+load_common prompt
+load_common no_implicit_make
+load_common git
+
+remote_repo="$(_shlomif_github "freecell-pro-0fc-deals")"
+git_base="$HOME"/Download/unpack/games/freecell/freecell-pro-0fc-deals
+this="$git_base"
+c="$this"
+
+cd "$this"
+
+c()
+{
+    cd "$c"
+}
+
+t()
+{
+    make total
+}
+
+prompt()
+{
+    __prompt_cmd \
+        "\$this=$this" \
+        "~=$HOME"
+}
+
+ci()
+{
+    git commit -F "$this/add-more-to-log-commit-msg.txt"
+}
+
+proj_name='0fc'
+
+t
