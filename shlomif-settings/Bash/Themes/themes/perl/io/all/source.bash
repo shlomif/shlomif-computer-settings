2fd15fd58fc2c6f4d2bb70d2caf60b7ce981b518
Shlomi Fish <shlomif@shlomifish.org>
2018-05-02 18:04:46 +0300

    convert to dzil

--- shlomif-settings/Bash/Themes/themes/perl/io/all/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/io/all/source.bash
@@ -11,73 +11,9 @@ this="$module"
 git_remote_shlomif="$(_shlomif_github "io-all-pm")"
 git_remote_ingy='git@github.com:ingydotnet/io-all-pm.git'
 
-# Make sure that gvim's filename completion ignores filenames that it should
-# not edit.
-
-__dist_name()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^name:" | sed 's/^name: *//'
-    )
-}
-
-__version()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^version:" | sed 's/^version: *//'
-    )
-}
-
-__check_for_distro()
-{
-    if [ -e "META.yml" ] ; then
-        return 0
-    else
-        echo "Not a distro dir" 1>&2
-        return 1
-    fi
-}
-
-__test_distribution()
-{
-    __check_for_distro &&
-    (
-        make disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
 __myctags()
 {
     ( cd "$trunk"/build/build-tags && bash build-ctags.sh )
-}
-
-cd $this
-
-__install_to_temp()
-{
-    (
-        make -f "$modules_makefile" "$(pwd)"
-    )
-}
-
-
-__prepare_install_all_to_temp_makefile()
-{
-    (cd "$build_scripts_dir" && perl create-makefile.pl)
-}
-
-__install_all_to_temp()
-{
-    target="$1"
-    shift
-    make_params=""
-    if [ "$target" == "runtest" ] ; then
-        make_params="$make_params TEST_TARGET=runtest"
-    fi
-    (
-        make -f "$modules_makefile" all $make_params
-    )
 }
 
 prompt()
@@ -100,3 +36,4 @@ setup()
 }
 
 proj_name='IO-All'
+cd $this
--- shlomif-settings/Bash/Themes/themes/perl/module/format/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/module/format/source.bash
@@ -1,88 +1,16 @@
 load_common mymake
 load_common completion
 load_common prompt
-load_common hg
+load_common git
 
 cpan_base="$HOME/progs/perl/cpan"
-hg_base="$cpan_base"
-repos="$cpan_base/hg"
+git_base="$cpan_base"
+repos="$cpan_base/git"
 trunk="$repos"
 base="$repos/Module-Format/Module-Format"
 this="$base"
-rw_repos_url="ssh://hg@bitbucket.org/shlomif/web-cpan"
-remote_repo="$rw_repos_url"
+remote_repo="$(_shlomif_github "perl-Module-Format")"
 
-# Make sure that gvim's filename completion ignores filenames that it should
-# not edit.
-
-__dist_name()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^name:" | sed 's/^name: //'
-    )
-}
-
-__version()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^version:" | sed 's/^version: //'
-    )
-}
-
-__test_distribution()
-{
-    (
-        cd "$this"
-        ./Build disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
-__run_integration_tests()
-{
-    (
-        touch "$test_dir"/head.pl ;
-        __display_integration_tests_results ;
-    )
-}
-
-__display_integration_tests_results()
-{
-    (
-        cd "$test_dir" ;
-        make ;
-    )
-}
-
-__rerun_coverage()
-{
-    (
-        cd "$this"
-        cover -delete
-        HARNESS_PERL_SWITCHES=-MDevel::Cover make test
-        cover
-    )
-}
-
-__run_build_tests()
-{
-    (
-        cd "$trunk/tests/build/EU-MM/" ;
-        ./test.sh ;
-    )
-}
-
-t()
-{
-    (cd "$this" ; ./Build runtest)
-}
-
-dt()
-{
-    (cd "$this" ; ./Build distruntest)
-}
-
-cd $this
 
 prompt()
 {
@@ -93,3 +21,4 @@ prompt()
 }
 
 proj_name='Module-Format'
+cd $this
