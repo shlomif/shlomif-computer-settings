1a6a97ef88ab7ceaf360e89a5bd83402b447348a
Shlomi Fish <shlomif@shlomifish.org>
2018-05-02 15:46:44 +0300

    convert to dzil

--- shlomif-settings/Bash/Themes/themes/xml/compare-ml/source.bash
+++ shlomif-settings/Bash/Themes/themes/xml/compare-ml/source.bash
@@ -2,7 +2,6 @@ load_common mymake
 load_common completion
 load_common prompt
 load_common hg
-# load_common gen_patch
 
 base="$HOME/progs/perl/cpan/XML/CompareML"
 hg_base="$base"
@@ -12,47 +11,6 @@ rw_repos_url="svn+ssh://svn.berlios.de/svnroot/repos/web-cpan/xml-compareml"
 read_repos_url="svn://svn.berlios.de/web-cpan/xml-compareml"
 
 remote_repo="$(_shlomif_bitbucket "xml-compareml")"
-# Make sure that gvim's filename completion ignores filenames that it should
-# not edit.
-
-make()
-{
-    local target
-    target="$1"
-    shift
-    if [ "$target" == "all" ] ; then
-        target=build
-    fi
-    ./Build "$target"
-}
-
-complete -o filenames -F __gvim_completion gvim
-
-__dist_name()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^name:" | sed 's/^name: //'
-    )
-}
-
-__version()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^version:" | sed 's/^version: //'
-    )
-}
-
-__test_distribution()
-{
-    (
-        cd "$this"
-        perl Build.PL
-        ./Build disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
-cd "$this"
 
 prompt()
 {
@@ -63,3 +21,4 @@ prompt()
 }
 
 proj_name='xml-compareml'
+cd "$this"
--- shlomif-settings/Bash/Themes/themes/xml/fortune/source.bash
+++ shlomif-settings/Bash/Themes/themes/xml/fortune/source.bash
@@ -11,63 +11,9 @@ this="$module"
 
 remote_repo="$(_shlomif_bitbucket "fortune-xml")"
 
-__dist_name()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^name:" | sed 's/^name: *//'
-    )
-}
-
-__version()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^version:" | sed 's/^version: *//'
-    )
-}
-
-__check_for_distro()
-{
-    if [ -e "META.yml" ] ; then
-        return 0
-    else
-        echo "Not a distro dir" 1>&2
-        return 1
-    fi
-}
-
-__test_distribution()
-{
-    __check_for_distro &&
-    (
-        make disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
 __myctags()
 {
     ( cd "$trunk"/build/build-tags && bash build-ctags.sh )
-}
-
-cd $this
-
-__install_to_temp()
-{
-    (
-        make -f "$modules_makefile" "$(pwd)"
-    )
-}
-
-__prepare_install_all_to_temp_makefile()
-{
-    (cd "$build_scripts_dir" && perl create-makefile.pl)
-}
-
-__install_all_to_temp()
-{
-    (
-        make -f "$modules_makefile" all
-    )
 }
 
 prompt()
@@ -80,3 +26,5 @@ prompt()
 }
 
 proj_name='xml-fortune'
+
+cd $this
