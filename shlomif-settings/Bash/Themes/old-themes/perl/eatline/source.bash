65e3adfc7e6206fc090c54cf1026d323bd431c04
Shlomi Fish <shlomif@shlomifish.org>
2018-05-02 18:20:35 +0300

    convert to dzil

--- shlomif-settings/Bash/Themes/old-themes/perl/eatline/source.bash
+++ shlomif-settings/Bash/Themes/old-themes/perl/eatline/source.bash
@@ -7,68 +7,6 @@ trunk="$base/trunk"
 module="$trunk/perl-prototype/Term-Eatline"
 this="$module"
 
-# Make sure that gvim's filename completion ignores filenames that it should
-# not edit.
-
-__dist_name()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^name:" | sed 's/^name: //'
-    )
-}
-
-__version()
-{
-    (cd "$this" &&
-        cat META.yml | grep "^version:" | sed 's/^version: //'
-    )
-}
-
-__test_distribution()
-{
-    (
-        cd "$this"
-        ./Build disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
-__run_integration_tests()
-{
-    (
-        touch "$test_dir"/head.pl ;
-        __display_integration_tests_results ;
-    )
-}
-
-__display_integration_tests_results()
-{
-    (
-        cd "$test_dir" ;
-        make ;
-    )
-}
-
-__rerun_coverage()
-{
-    (
-        cd "$this"
-        cover -delete
-        HARNESS_PERL_SWITCHES=-MDevel::Cover make test
-        cover
-    )
-}
-
-__run_build_tests()
-{
-    (
-        cd "$trunk/tests/build/EU-MM/" ;
-        ./test.sh ;
-    )
-}
-
-cd $this
-
 prompt()
 {
     __prompt_cmd \
@@ -77,5 +15,4 @@ prompt()
         "\$base=$base" \
         "~=$HOME"
 }
-
-PS1="\\u:\$(prompt)\\$ "
+cd $this
--- shlomif-settings/Bash/Themes/old-themes/riddles/sokoban/source.bash
+++ shlomif-settings/Bash/Themes/old-themes/riddles/sokoban/source.bash
@@ -16,48 +16,10 @@ remote_repo="$($__themes_dir/common/bitbucket-hg-remote-repo.pl --user="shlomif"
 inst_modules_dir="$HOME/apps/perl/modules"
 modules_makefile="${build_scripts_dir}/Modules.mak"
 
-# Make sure that gvim's filename completion ignores filenames that it should
-# not edit.
-
-__dist_name()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^name:" | sed 's/^name: *//'
-    )
-}
-
-__version()
-{
-    (__check_for_distro &&
-        cat META.yml | grep "^version:" | sed 's/^version: *//'
-    )
-}
-
-__check_for_distro()
-{
-    if [ -e "META.yml" ] ; then
-        return 0
-    else
-        echo "Not a distro dir" 1>&2
-        return 1
-    fi
-}
-
-__test_distribution()
-{
-    __check_for_distro &&
-    (
-        make disttest
-        rm -fr "$(__dist_name)-$(__version)"
-    )
-}
-
 __myctags()
 {
     ( cd "$this" && ctags -R )
 }
-
-cd $this
 
 prompt()
 {
@@ -67,5 +29,4 @@ prompt()
         "\$base=$base" \
         "~=$HOME"
 }
-
-PS1="\\u:\$(prompt)\\$ "
+cd $this
