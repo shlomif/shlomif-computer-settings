0d1ec3ea9ef14501c250b2a7148637da6d6ec6b8
Shlomi Fish <shlomif@shlomifish.org>
2018-05-26 12:46:56 +0300

    Add the monkeys script - a joke.

---
+++ shlomif-settings/home-bin-executables/bin/unix-shakespeare-monkeys.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# unix-shakespeare-monkeys.bash
+#
+# Distributed under terms of the MIT license.
+#
+# Based on https://www.mail-archive.com/linux-il@cs.huji.ac.il/msg66033.html
+# Omer Zak's signature.
+< /dev/urandom strings -n 16
