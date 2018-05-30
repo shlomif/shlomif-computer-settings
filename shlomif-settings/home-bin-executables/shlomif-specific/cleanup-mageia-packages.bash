ff988a816b0714412569ab127ccb14e81f4fdb3e
Shlomi Fish <shlomif@shlomifish.org>
2018-05-30 09:27:18 +0300

    Add an rpms cleanup script.

---
+++ shlomif-settings/home-bin-executables/shlomif-specific/cleanup-mageia-packages.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# cleanup-mageia-packages.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+e task-shlomif-homesite
+e task-shlomif-fizzbuzz
+e perl-Test-XML
+e glibc-devel
+e python2-numpy
+e ruby
+e ruby-irb
+e wml
+e $(rpm -qa 'lib64opencv*')
+e gnome-control-center
+e git-core
+e netprofile
+e sselp
+e perl-Mojolicious
+eo
