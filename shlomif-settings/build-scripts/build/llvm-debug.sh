ceb96fd152e8d6611e62a05da81970b9bbb4f207
Shlomi Fish <shlomif@shlomifish.org>
2018-06-06 00:18:53 +0300

    update the llvm scripts.

--- shlomif-settings/build-scripts/build/llvm-debug.sh
+++ shlomif-settings/build-scripts/build/llvm-debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE="debug" -DCMAKE_INSTALL_PREFIX="$HOME/apps/llvm" ../
+cmake -DCMAKE_BUILD_TYPE="debug" -DCMAKE_INSTALL_PREFIX="$HOME/apps/llvm" "$@"
--- shlomif-settings/vcs-checkout-scripts/scripts/llvm.sh
+++ shlomif-settings/vcs-checkout-scripts/scripts/llvm.sh
@@ -7,3 +7,15 @@
 #
 
 svn co http://llvm.org/svn/llvm-project/llvm/trunk
+(
+    cd trunk/tools
+    svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+)
+(
+    cd trunk/tools/clang/tools
+    svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+)
+(
+    cd trunk/projects
+    svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+)
