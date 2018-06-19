341892675f821ace41d1cbf5c2ce48ff7553aa28
Shlomi Fish <shlomif@shlomifish.org>
2018-06-19 16:36:01 +0300

    Add lepton.

---
+++ shlomif-settings/build-scripts/build/lepton.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# lepton.sh
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/lepton" "$@"
