0aba29f45c0737f867de5f92cc1c3c938c4052dd
Shlomi Fish <shlomif@shlomifish.org>
2018-07-18 22:22:18 +0300

    update.

--- shlomif-settings/build-scripts/build/librsvg-for-gimp-2.7.x.sh
+++ shlomif-settings/build-scripts/build/librsvg-for-gimp-2.7.x.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export PKG_CONFIG_PATH=/usr/local/apps/gtk-2.4.x/lib/pkgconfig/
-export LD_LIBRARY_PATH=/usr/local/apps/gtk-2.4.x/lib:"$LD_LIBRARY_PATH"
-LDFLAGS="-L/usr/local/apps/gtk-2.4.x/lib" \
-    ./configure --prefix=/usr/local/apps/gtk-2.4.x/ \
+basedir=/usr/local/apps/gtk-2.4.x
+basedir=$HOME/apps/gtk-2.4.x
+export PKG_CONFIG_PATH="$basedir"/lib/pkgconfig/
+export LD_LIBRARY_PATH="$basedir"/lib:"$LD_LIBRARY_PATH"
+LDFLAGS="-L$basedir/lib" \
+    ./configure --prefix=$basedir \
     --with-svgz=no
