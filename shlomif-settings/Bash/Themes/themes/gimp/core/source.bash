6bb37931fc167e5fafeaca101a7412ff36a3cadc
Shlomi Fish <shlomif@shlomifish.org>
2018-08-09 11:27:08 +0300

    remove old

--- shlomif-settings/Bash/Themes/themes/gimp/core/source.bash
+++ shlomif-settings/Bash/Themes/themes/gimp/core/source.bash
@@ -13,15 +13,6 @@ proj_name="gimp"
 export PKG_CONFIG_PATH="$HOME/apps/graphics/gegl/lib/pkgconfig:$HOME/apps/graphics/babl/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$HOME/apps/graphics/gegl/lib:$HOME/apps/graphics/babl/lib:$LD_LIBRARY_PATH"
 
-__Vipe_upload_patch()
-{
-    local last_rev
-    last_rev="$(__get_latest_patch_rev)" ;
-    rsync --rsh=ssh -v --progress "$(__get_patch_path $last_rev)" "shlomif@vipe:public_html/gimp/gimpressionist/"
-}
-
-cd $this
-
 prompt()
 {
     __prompt_cmd \
@@ -30,3 +21,5 @@ prompt()
         "\$base=$base" \
         "~=$HOME"
 }
+
+cd "$this"
