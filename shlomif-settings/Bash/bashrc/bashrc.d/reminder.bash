df07ba1d97d8b951f581e9bd30c9273b76412062
Shlomi Fish <shlomif@shlomifish.org>
2018-08-24 21:44:00 +0300

    place the reminder on every prompt.

--- shlomif-settings/Bash/bashrc/bashrc.d/reminder.bash
+++ shlomif-settings/Bash/bashrc/bashrc.d/reminder.bash
@@ -1,8 +1,14 @@
 reminder_file="$HOME/.local/share/shlomif/reminder/reminder.txt"
-if [ -s "$reminder_file" ] ; then
-    perl -Mstrict -Mwarnings -MTerm::ANSIColor -E 'my $x = colored("===", "bold red"); say "$x Reminders $x"'
-    cat "$reminder_file"
-fi
+
+__display_reminder()
+{
+    if [ -s "$reminder_file" ] ; then
+        perl -Mstrict -Mwarnings -MTerm::ANSIColor -E 'my $x = colored("===", "bold red"); say "$x Reminders $x"'
+        cat "$reminder_file"
+    fi
+}
+__display_reminder
+export PROMPT_COMMAND='__display_reminder'
 
 Cancel_reminder()
 {
