29d1f697d562d01ab68046af201219ebf54c622f
Shlomi Fish <shlomif@shlomifish.org>
2018-09-04 10:31:07 +0300

    add aliases.

--- shlomif-settings/Bash/Themes/themes/perl/6/source.bash
+++ shlomif-settings/Bash/Themes/themes/perl/6/source.bash
@@ -9,7 +9,8 @@ export RAKUDO_ROOT=${RAKUDO_ROOT:-$rs}
 export PATH="${RAKUDO_ROOT}/bin:$PATH"
 PATH="$PATH:$HOME/Download/unpack/perl/p6/rakudo-star/rakudo-star-2018.04/install/bin:$HOME/Download/unpack/perl/p6/rakudo-star/rakudo-star-2018.04/install/share/perl6/site/bin"
 
-trunk="$base/doc"
+# trunk="$base/doc"
+trunk="$base/CI-Gen"
 app_dir="$trunk"
 this="$app_dir"
 
@@ -35,3 +36,25 @@ rgm()
 }
 
 proj_name='Perl6'
+
+t()
+{
+    mi6 test
+}
+
+ci()
+{
+    git add -u . && \
+    git commit "$@"
+}
+
+i()
+{
+    zef install --force-install . && \
+    rgm zef install --force-install .
+}
+
+e()
+{
+    gvim -o lib/CI/Gen.pm6 t/01-basic.t bin/ci-generate
+}
