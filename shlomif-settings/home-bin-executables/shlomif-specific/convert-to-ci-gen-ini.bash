e6d66f17030cec4ac0256b439c024a69ee0884b1
Shlomi Fish <shlomif@shlomifish.org>
2018-09-04 23:27:02 +0300

    add a theme

---
+++ shlomif-settings/home-bin-executables/shlomif-specific/convert-to-ci-gen-ini.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+#
+# regen-all-gen-ci.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+locate --regex '/\.travis\.yml$' | xargs dirname | perl -MSQ -lnE 'use IO::All; my $f = "$_/.gen-ci.bash"; my $n = "$_/.ci-gen.ini"; if (-f $f and 1) { say "== $_ ; $f =="; $t = io($f)->all; ($the) = $t =~ /--theme\s+(\S+)/ms or die "foo"; ($p)= $t =~ /--param $S(\S+?)$S/; io($n)->print("theme = $the\n", ($p ? "\n[param]\n\n" . ($p =~ s/=/ = /r) . "\n" : "")); say ` cd $_ ; git add .ci-gen.ini; ci-generate ; git diff`}' 2>&1 | gvim -
