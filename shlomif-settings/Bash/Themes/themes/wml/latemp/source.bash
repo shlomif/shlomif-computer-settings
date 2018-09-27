eb79b37945bed546b37cc1f0de32f8e58ed0f4af
Shlomi Fish <shlomif@shlomifish.org>
2018-09-27 10:35:19 +0300

    zsh.

--- shlomif-settings/Bash/Themes/themes/wml/latemp/source.bash
+++ shlomif-settings/Bash/Themes/themes/wml/latemp/source.bash
@@ -6,13 +6,13 @@ trunk="$git_base/trunk"
 this="$trunk/installer"
 site="$trunk/site/wml"
 # test_dir="$trunk/tests/integration/sites-gen"
-modules="$trunk/Perl/modules"
+modules_="$trunk/Perl/modules"
 
 remote_repo="$(_thewml_github "latemp")"
 
 prompt()
 {
-    __prompt_cmd "\$modules=$modules" "\$trunk=$trunk" "\$base=$base" "~=$HOME"
+    __prompt_cmd "\$modules_=$modules_" "\$trunk=$trunk" "\$base=$base" "~=$HOME"
 }
 
 proj_name='latemp'
