1fc984a3fc2823c3c70756f222e584bbb5637eeb
Shlomi Fish <shlomif@shlomifish.org>
2018-10-13 03:44:45 +0300

    install under /opt so other users can use.

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -11,4 +11,6 @@ if test "$NO_NINJA" = 1 ; then
     ninja=''
 fi
 
-cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$HOME/apps/graphics/inkscape-trunk" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON $flags "$@"
+prefix="$HOME/apps/graphics/inkscape-trunk"
+prefix="/opt/vlc-3.0/inkscape-trunk"
+cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON $flags "$@"
