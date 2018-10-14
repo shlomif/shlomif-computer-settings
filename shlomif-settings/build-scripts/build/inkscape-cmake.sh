9d8f3ca5f888d81203fee958baadf26e13fe847a
Shlomi Fish <shlomif@shlomifish.org>
2018-10-14 23:34:38 +0300

    disable jemalloc due to bugs.

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -13,4 +13,4 @@ fi
 
 prefix="$HOME/apps/graphics/inkscape-trunk"
 prefix="/opt/vlc-3.0/inkscape-trunk"
-cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON $flags "$@"
+cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON -DWITH_JEMALLOC=OFF $flags "$@"
