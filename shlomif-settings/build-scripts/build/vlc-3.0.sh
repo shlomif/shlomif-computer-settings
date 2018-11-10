3dc6daf43b3cf44616de69b56df70cf219ef03c8
Shlomi Fish <shlomif@shlomifish.org>
2018-11-10 16:38:04 +0200

    update

--- shlomif-settings/build-scripts/build/vlc-3.0.sh
+++ shlomif-settings/build-scripts/build/vlc-3.0.sh
@@ -2,6 +2,7 @@
 p="/opt/vlc-3.0"
 export LUAC="/usr/bin/luac5.2"
 export CFLAGS='-g'
+export PKG_CONFIG_PATH="$p/lib/pkgconfig"
 ./configure --prefix="$p" --with-kde-solid="$p/share/apps/solid" --enable-debug
 
 # --disable-schroedinger --disable-bluray --disable-swscale --enable-debug
