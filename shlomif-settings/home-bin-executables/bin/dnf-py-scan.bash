101e86414f3eefd755b4e650aec3cd8843da4c75
Shlomi Fish <shlomif@shlomifish.org>
2019-01-07 20:34:53 +0200

    add dnf python3 scan.

---
+++ shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+#
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+( for i in $(dnf repoquery --qf "%{NAME}" 'python3-*' | grep -vP 'aeidon|^python3-(async|aws-cli|backports.os)$' | perl -lnE 'say if /^python3-b/..1') ; do dnf -y install "$i" || exit ; done)
