7654f3c44bfa96a3c3ccd7b082e9b1764da04546
Shlomi Fish <shlomif@shlomifish.org>
2019-01-09 18:48:19 +0200

    dnf scan progress

--- shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
+++ shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
@@ -4,4 +4,4 @@
 #
 # Distributed under terms of the MIT license.
 #
-( for i in $(dnf repoquery --qf "%{NAME}" 'python3-*' | grep -vP 'aeidon|^python3-(async|aws-cli|backports.os|celery|cherrypy|docker-py|docker-scripts|getdist|kombu|libguestfs|(?:(?:django|dogpile-|ffmpeg-|font|jupyter).*))$' | perl -lnE 'say if /^python3-ma/..1') ; do dnf -y install "$i" || urpmi --auto "$i" || exit ; done)
+( for i in $(dnf repoquery --qf "%{NAME}" 'python3-*' | grep -vP 'aeidon|^python3-(async|aws-cli|backports.os|celery|cherrypy|docker-py|docker-scripts|getdist|kombu|libguestfs|mpi4py|mpi4py-devel|ndg-httpsclient|odfpy|opencv|pandas|(?:(?:django|dogpile-|ffmpeg-|font|jupyter).*))$' | perl -lnE 'say if /^python3-pa/..1') ; do dnf -y install "$i" || urpmi --auto "$i" || exit ; done)
