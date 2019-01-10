7524f750b0076625bd5cefe9194be8f96d5ea51b
Shlomi Fish <shlomif@shlomifish.org>
2019-01-10 19:55:49 +0200

    dnf / python /py3.7/mga scan progress

--- shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
+++ shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
@@ -4,4 +4,4 @@
 #
 # Distributed under terms of the MIT license.
 #
-( for i in $(dnf repoquery --qf "%{NAME}" 'python3-*' | grep -vP 'aeidon|^python3-(async|aws-cli|backports.os|celery|cherrypy|docker-py|docker-scripts|getdist|kombu|libguestfs|mpi4py|mpi4py-devel|ndg-httpsclient|odfpy|opencv|pandas|plplot|protorpc|py2pack|pymc|qgis|(?:(?:django|dogpile-|ffmpeg-|font|jupyter|pycrypto|pytest-django).*))$' | perl -lnE 'say if /^python3-q/..1') ; do dnf -y install "$i" || urpmi --auto "$i" || exit ; done)
+( for i in $(dnf repoquery --qf "%{NAME}" 'python3-*' | grep -vP 'aeidon|^python3-(async|aws-cli|backports.os|celery|cherrypy|docker-py|docker-scripts|getdist|kombu|libguestfs|mpi4py|mpi4py-devel|ndg-httpsclient|odfpy|opencv|pandas|plplot|protorpc|py2pack|pymc|qgis|qstk|sleekxmpp|tqdm|socksipy-branch|sphinxcontrib-trio|trollius|trollius-redis|trololio|(?:(?:django|dogpile-|ffmpeg-|font|jupyter|pycrypto|pytest-django|rust|scikit).*))$' | perl -lnE 'say if /^python3-u/..1') ; do dnf -y install "$i" || urpmi --auto "$i" || exit ; done)
