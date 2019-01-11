e8e9439a273c0c047c55164f7937ae927fee678f
Shlomi Fish <shlomif@shlomifish.org>
2019-01-11 21:09:08 +0200

    silence errors that become new mail.

--- shlomif-settings/home-bin-executables/shlomif-specific/backup-firefox-dir.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/backup-firefox-dir.bash
@@ -3,4 +3,4 @@ cd "$HOME"
 d="$HOME/Backup/firefox-config/old-configs/"
 mkdir -p "$d"
 fn="$d/$(date +"%Y-%m-%d-%H").tar.xz"
-nice -19 tar -caf "$fn" .mozilla
+nice -19 tar -caf "$fn" .mozilla 2>/dev/null
