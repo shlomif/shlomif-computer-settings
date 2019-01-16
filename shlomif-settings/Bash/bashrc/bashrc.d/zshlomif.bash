b62f64a5accdb6c87062abe7558ae1e3b26163a8
Shlomi Fish <shlomif@shlomifish.org>
2019-01-16 23:24:11 +0200

    add to path.

--- shlomif-settings/Bash/bashrc/bashrc.d/zshlomif.bash
+++ shlomif-settings/Bash/bashrc/bashrc.d/zshlomif.bash
@@ -23,4 +23,4 @@ then
     export QT_QPA_PLATFORMTHEME="qt5ct"
 fi
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/apps/to-del-ncurses/lib"
-PATH="$HOME/apps/golang/bin:$HOME/.local/bin:$PATH"
+PATH="$HOME/apps/golang/bin:$HOME/.local/bin:$HOME/.perl6/bin:$PATH"
