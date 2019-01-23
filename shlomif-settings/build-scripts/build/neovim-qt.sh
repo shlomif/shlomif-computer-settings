b5ad5d0942cf137e077fb37776acab11603b8d88
Shlomi Fish <shlomif@shlomifish.org>
2019-01-23 12:47:50 +0200

    avoid warnings.
    See https://github.com/equalsraf/neovim-qt/issues/509 .

--- shlomif-settings/build-scripts/build/neovim-qt.sh
+++ shlomif-settings/build-scripts/build/neovim-qt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/neovim" "$1"
 # echo "You should really be using the bootstrapping makefile" 1>&2
-cmake -DCMAKE_INSTALL_PREFIX:PATH="$HOME/apps/neovim" "$@"
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="$HOME/apps/neovim" "$@"
