efb9e01d6911defae4935f7727076c72d1c64c09
Shlomi Fish <shlomif@shlomifish.org>
2019-02-10 20:38:30 +0200

    add rshasum

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/rshasum.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# rshasum.sh
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+git clone -b add-bash-vresion "https://github.com/shlomif/rshasum"
