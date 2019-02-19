6b0a1a4afd84faec3a41c6efdf354aa8879c2206
Shlomi Fish <shlomif@shlomifish.org>
2019-02-19 18:24:04 +0200

    defaults.

--- shlomif-settings/build-scripts/build/vlc-3.0-opt.sh
+++ shlomif-settings/build-scripts/build/vlc-3.0-opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 p="/opt/vlc-3.0"
-export LUAC="/usr/bin/luac5.2"
-export CFLAGS='-O3 -march=native'
+export LUAC="${LUAC:-/usr/bin/luac5.2}"
+export CFLAGS="${CFLAGS:--O3 -march=native}"
 ./configure --prefix="$p" --with-kde-solid="$p/share/apps/solid" --enable-debug --disable-opencv
 
 # --disable-schroedinger --disable-bluray --disable-swscale --enable-debug
