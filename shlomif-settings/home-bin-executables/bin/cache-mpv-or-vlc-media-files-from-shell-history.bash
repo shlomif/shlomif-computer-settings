5a79c164762eb9d4587f26758c633ef1237a832b
Shlomi Fish <shlomif@shlomifish.org>
2019-02-23 10:31:04 +0200

    start adding

---
+++ shlomif-settings/home-bin-executables/bin/cache-mpv-or-vlc-media-files-from-shell-history.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# cache-mpv-or-vlc-media-files-from-shell-history.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+set -e -x
+cachedir=~/Download/Video/media-cache
+
+if ! test -e "$cachedir"
+then
+    mkdir -p "$cachedir"
+fi
+cat | perl -lanE 'say grep { m%^/% } @F if $F[1] =~ /\A(?:vlc|mpv)\z/'
+
