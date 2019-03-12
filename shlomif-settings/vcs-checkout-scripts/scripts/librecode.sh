19ecf2a28014894ca64a4ec793153b4e60ce0a22
Shlomi Fish <shlomif@shlomifish.org>
2019-03-12 16:18:21 +0200

    add

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/librecode.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# rshasum.sh
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+git clone https://github.com/rrthomas/Recode
