bc6ef6ddd68d5ea6129ef47a6af0fa457f6d7b30
Shlomi Fish <shlomif@shlomifish.org>
2019-03-16 20:49:44 +0200

    add a flag.

--- shlomif-settings/build-scripts/build/gimp-from-svn.sh
+++ shlomif-settings/build-scripts/build/gimp-from-svn.sh
@@ -3,6 +3,7 @@ gegl_p="$HOME/apps/graphics/gegl"
 export PATH="$gegl_p/bin:$PATH"
 export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$HOME/apps/graphics/libmypaint/lib/pkgconfig:$HOME/apps/graphics/babl/lib/pkgconfig:$PKG_CONFIG_PATH"
 CFLAGS="-g" \
-    ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode
+    ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode \
+        --with-jpeg2000
 
 # --enable-gtk-doc \
