98b13cc1935f9afc16e76f212c901d0209b937f5
Shlomi Fish <shlomif@shlomifish.org>
2019-03-18 14:41:24 +0200

    add the 11:11 reminder.

---
+++ shlomif-settings/home-bin-executables/shlomif-specific/reddit11-11.bash
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+#
+# reddit11-11.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+cdown-to 'TZ="America/New_York" 11:09:30' ; notifier notify --msg "11:11 on #reddit"
