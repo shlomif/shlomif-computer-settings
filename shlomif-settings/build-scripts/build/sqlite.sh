edbd14bc599f741cda3a029cd37d3b8969685ff6
Shlomi Fish <shlomif@shlomifish.org>
2019-03-21 19:37:30 +0200

    add sqlite

---
+++ shlomif-settings/build-scripts/build/sqlite.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# sqlite.sh
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+./configure --prefix="$HOME/apps/to-del-sqlite" # --enable-maintainer-mode
