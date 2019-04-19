fd78986748cc382065dd1ec438d4a3cea1ae8d8d
Shlomi Fish <shlomif@shlomifish.org>
2019-04-19 13:07:47 +0300

    add more

--- shlomif-settings/shell-history/history.bash
+++ shlomif-settings/shell-history/history.bash
@@ -4,16 +4,22 @@
 -t fcs
 -t homepage
 -t rpm/mag
+-t wml/itself
+. ~/bin/Dev-Path-Configs-Source-Me.bash
 ./gen-helpers
 Cancel_reminder
 b
+bash bin/rebuild
 c7
 cd -
 cd ..
 ci-generate
 claws-mail
 d
+date
 did
+dolphin ~/Music/dosd-mp3s/ ~/Music/mp3s/ ~/Download/Video/
+du .
 dzil build
 dzil release
 dzil test --all
@@ -24,6 +30,7 @@ fmt && pt
 fmt && t
 fortune
 git au
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/extract-method.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/fix-markup-valid.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/refactoring.txt
 git clean -dxf .
@@ -36,9 +43,13 @@ git pushu
 git rebase master
 git s o
 git s u
+git stash
+git stash pop
+git tag -l
 gvim
 gvim +mak
 gvim .travis.yml
+gvim dist.ini
 gvim lib/make/main.mak
 hc
 hexchat
@@ -54,8 +65,13 @@ ls -lrt
 ls -rt
 m
 make
+make -j8
+make PROD=1 upload
+make clean
 make test
 make upload
+p --tags
+perl ../scripts/cmd-line-compiler compile && m && y
 perl ../scripts/multi_config_tests.pl ; n --msg 'fcs test'
 pidgin
 ping www.google.com
@@ -66,6 +82,9 @@ rake
 rake prettier
 rake test
 rebuild
+rehash
+rm -fr RPMS/ SRPMS/ BUILD BUILDROOT/
+run
 sd
 sub
 svn di
