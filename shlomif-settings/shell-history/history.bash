68c26942d480637e51f4cff44710efe378944e47
Shlomi Fish <shlomif@shlomifish.org>
2019-04-19 12:51:19 +0300

    add more

--- shlomif-settings/shell-history/history.bash
+++ shlomif-settings/shell-history/history.bash
@@ -1,17 +1,26 @@
+-t 0fc
+-t euler
 -t fcs
+-t homepage
 -t rpm/mag
 ./gen-helpers
 Cancel_reminder
 b
+c7
 cd -
 cd ..
+claws-mail
+d
+did
 dzil build
 dzil release
 dzil test --all
 e
+et
 fmt
 fmt && pt
 fmt && t
+fortune
 git au
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/refactoring.txt
 git clean -dxf .
@@ -20,11 +29,20 @@ git dh
 git dhs
 git i
 git push
+git pushu
+git rebase master
+git s u
 gvim
+gvim +mak
 hc
+hexchat
+hg pull
+hg push
+hg st
 hm
 k5
 ls
+ls -l
 ls -lrS
 ls -lrt
 ls -rt
@@ -32,11 +50,18 @@ m
 make
 make test
 make upload
+perl ../scripts/multi_config_tests.pl ; n --msg 'fcs test'
+pidgin
 ping www.google.com
+pm-suspend
 pt
+python3 Tests/validate-html-using-vnu.py
+rake prettier
+rake test
 rebuild
 sd
 sub
+svn st
 t
 tmux
 tmux a
