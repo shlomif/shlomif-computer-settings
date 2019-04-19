a6ac1faccf7a9068de947d25ffe9c33e36118cf6
Shlomi Fish <shlomif@shlomifish.org>
2019-04-19 12:40:55 +0300

    add more

--- shlomif-settings/shell-history/history.bash
+++ shlomif-settings/shell-history/history.bash
@@ -1,10 +1,21 @@
+-t fcs
+-t rpm/mag
+./gen-helpers
+Cancel_reminder
 b
+cd -
 cd ..
+dzil build
+dzil release
+dzil test --all
 e
 fmt
 fmt && pt
 fmt && t
 git au
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/refactoring.txt
+git clean -dxf .
+git clean -dxf ..
 git dh
 git dhs
 git i
@@ -14,13 +25,23 @@ hc
 hm
 k5
 ls
+ls -lrS
+ls -lrt
+ls -rt
 m
 make
+make test
+make upload
 ping www.google.com
 pt
+rebuild
 sd
+sub
 t
 tmux
 tmux a
 ts
+ub
+up
+ur
 uu
