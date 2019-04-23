66aefac5311e95ccfae7c55b1a0b6544a0fc31d7
Shlomi Fish <shlomif@shlomifish.org>
2019-04-23 21:02:48 +0300

    add more

--- shlomif-settings/shell-history/history.bash
+++ shlomif-settings/shell-history/history.bash
@@ -1,4 +1,5 @@
 -t 0fc
+-t better-scm
 -t dotfiles
 -t euler
 -t fcs
@@ -6,22 +7,53 @@
 -t games/fortune
 -t games/pysol
 -t homepage
+-t inkscape
+-t nvim
+-t perl/begin
+-t perl/core
+-t perl/xml/libxml
 -t qp
 -t rpm/mag
 -t sys/cleanup
+-t todo
+-t vim/begin
+-t wml/cookie
 -t wml/itself
+-t wml/latemp
+. bin/spell.bash
 . ~/bin/Dev-Path-Configs-Source-Me.bash
 ../scripts/Tatzer -l extra_speed2
+../scripts/Tatzer -l n2t
 ../source/Tatzer
+../source/Tatzer -l extra_speed2
 ../source/Tatzer -l n2t
+./a.out
+./arcanist/bin/arc diff master
 ./bwbasic fizz-buzz.bas | gvim -
 ./gen-helpers
 ./kpat
 ./node_modules/.bin/tsc abc-path.ts | gvim -
+/home/shlomif/Download/unpack/prog/python/pypy3.5-6.0.0-linux_x86_64-portable/bin/pypy3 396-v1.py
+/home/shlomif/Download/unpack/prog/python/pypy3.5-6.0.0-linux_x86_64-portable/bin/pypy3 650/650-v1.py
+/home/shlomif/Download/unpack/prog/python/pypy3.5-6.0.0-linux_x86_64-portable/bin/pypy3 gen_divs.py
+/home/shlomif/apps/julia-1.1.0/bin/julia 658/e658_v3.jl
+/home/shlomif/apps/julia-1.1.0/bin/julia 658/e658_v3.jl | tee -a jlog2.txt | timestamper
+/home/shlomif/apps/julia-1.1.0/bin/julia 658/e658_v3.jl | tee -a jlog3.txt
+/home/shlomif/apps/julia-1.1.0/bin/julia e658_v4.jl
+/usr/bin/time bin/rebuild
 Cancel_reminder
+FCS_TEST_BUILD=1 t
+FCS_TEST_SHELL=1 perl ../source/run-tests.pl
+L
 NODE_PATH="`pwd`"/lib/for-node/js /usr/bin/node `which qunit-cli` lib/for-node/test-code.js
+NODE_PATH="`pwd`"/lib/for-node/js qunit lib/for-node/test-code-emcc.js
+NODE_PATH="`pwd`"/lib/for-node/js qunit lib/for-node/test-code.js
 NODE_PATH="`pwd`"/lib/for-node/js qunit-cli lib/for-node/test-code-emcc.js
+NODE_PATH="`pwd`"/lib/for-node/js qunit-cli lib/for-node/test-code.js | gvim -
+PATH+=:/home/shlomif/Download/unpack/prog/python/pypy3.5-6.0.0-linux_x86_64-portable/bin
+PATH+=:/home/shlomif/apps/perl6-rakudo/bin
 PATH="/home/shlomif/apps/golang/bin:/home/shlomif/.local/bin:/home/shlomif/apps/test/wml/bin:$PATH"
+PYTHONPATH="`pwd`/src:$PWD" python3 Tests/validate-html-using-vnu.py
 VirtualBox
 WD=awe startx
 WD=cin startx
@@ -32,50 +64,88 @@ WD=xfce startx
 a
 ah
 alias p='git push'
+an
 b
 b ; n --msg bm
 ba
+bash
+bash --rcfile build.sh
+bash -x bin/install-npm-deps.sh
+bash ../scripts/c-subroutine-metrics-line-count.bash
+bash ../scripts/fuzz-build.bash
+bash ../scripts/fuzz-build.bash g ; n -m fuzz
 bash ../scripts/split-fcc--all-in-one.bash
 bash .gen-ci.bash
 bash /home/shlomif/Backup/Arcs/backup.bash
 bash /home/shlomif/conf/build/gimp-git-all-deps.bash
+bash /home/shlomif/conf/build/perl/dzil-install.bash
 bash /home/shlomif/conf/build/perl/install-to-apps-perl-modules.sh
+bash /home/shlomif/conf/build/pidgin.sh
 bash /home/shlomif/conf/trunk/shlomif-settings/home-bin-executables/bin/trim-installed-rpms.bash
 bash /home/shlomif/conf/trunk/shlomif-settings/home-bin-executables/shlomif-specific/reddit11-11.bash
+bash 658/par2.bash | timestamper | tee -a ts2.txt
+bash 660/660_v2-wrap.bash
+bash bin/install-npm-deps.sh
 bash bin/rebuild
+bash fr.bash
 bash fr.bash 2>l
+bash myrsync.bash
 bash solve-more-3.bash | commify | timestamper
 bash ~/bin/backup-total.sh ; bash ~/bin/backup-extra-data.sh ; bash ~/bin/backup-total.sh ; bash ~/bin/backup-extra-data.sh ; n --msg "Backup finished"
+bat f
 bg
 bm -l -s
 bpat
 c
 c7
+caddy XorShift128Plus.pl 2>&1 | gvim -
 caddy XorShift128Plus.py
+cargo build
 cd
 cd -
 cd ..
+cd ../..
 cd ../b
+cd BUILD
+cd Backup
+cd Docs/programming/irc/freenode-##programming/faq/FreenodesprogrammingWiki
+cd build
+cd conf/trunk/
+cd dest
 cd lib/repos/Solitairey
+cd src
+cd todel
 cd todel/
 cdown-to 18:09:40 ; n --msg "11:11 on #Reddit"
 cdp
+ci
 ci -m '- Add BRs'
+ci -m '- Rebuild for new perl 5.28.0'
+ci -m '- Rebuild for python3 3.7'
 ci-generate
 claws-mail
 cmake -DWITH_GOLF_SOLVER=1 -DCMAKE_BUILD_TYPE=Release ../kpat
+cmp dest/web-forums/index.html ../dest/web-forums/index.html
 conf
 cpan-upload HTML-T5-0.001.tar.gz
 cpanp -i App::cpanminus
 cpanp -i Task::BeLike::SHLOMIF ; n -m cpan
 cs
 ct
+ctags -R .
 d
 date
 df
 did
+diff -u -r dest ../dest/ | gvim -
+diff -u -r ~/Backup/Arcs/post-dest/post-dest/ post-dest/ | gvim -
+disp
 dolphin ~/Music/dosd-mp3s/ ~/Music/mp3s/ ~/Download/Video/
 du -ah . | sort -h
+du -h . | sort -h
+du -ha . | sort -h
+du -s *
+du -sh *
 du .
 du .git
 dzil authordeps | xargs cpan -i
@@ -84,38 +154,83 @@ dzil release
 dzil test --all
 e
 eo
+eslint src/js/solver-freecell.js
 et
+export CC=/home/shlomif/bin/clang CXX=/home/shlomif/bin/clang++ FCS_CLANG=1
+export NOTIFIER_TO=lap
+export PATH="/home/shlomif/.local/bin:/home/shlomif/apps/test/wml/bin:$PATH"
 export PATH="/home/shlomif/apps/golang/bin:/home/shlomif/.local/bin:/home/shlomif/apps/test/wml/bin:$PATH"
 export VIMSYNT=1
+export XZ_OPT="-T4"
+fd wml src | xargs gvim -p
+find
 finish-server
+firefox
 firefox -no-remote
+firefox -no-remote 'http://localhost/shlomif/temp-Solitairey/dest/'
 flake8 .
 fmt
 fmt && pt
 fmt && t
+force_u_rpms
 fortune
 g
+g++ -o e451.exe -Wall -Wextra -O3 -march=native -flto -fwhole-program e451-quasisphere.cpp
+g++ -o e451.exe -Wall -Wextra -O3 -march=native -flto -fwhole-program e662-james-rauen.cpp  -Ofast
+gdb -args ./black-hole-solve --game black_hole --display-boards --rank-reach-prune ../c-solver/t/data/26464608654870335080.bh.board.txt
 gdb ./kpat
+get_nums | summary
+ghc -O2 mniip.hs
+git  i
+git add .tidyallrc
 git au
 git branch -l
+git branch -l -a
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/add-ci-gen-travis-yml.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/add-missing-deps-to-ci.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/convert-var-to-slot.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/extract-a-class.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/extract-a-module.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/extract-common-expr-into-var.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/extract-method.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/fix-grammar.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/fix-markup-valid.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/move-to-cookiecutter-latemp.txt
 git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/refactoring.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/rename-vars.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/semantic-markup.txt
+git ci -F ~/conf/trunk/shlomif-settings/git/commit-messages/update-ci-gen-travis-yml.txt
+git ci -m 'Convert pages to tt2'
+git ci -m 'add more'
 git ci -m 'add to gitignore'
+git ci -m 'bump version'
 git ci -m 'convert more pages to (x)html5'
+git ci -m 'convert more pages to x/html5'
+git ci -m 'eslint'
+git ci -m 'fix formatting'
+git ci -m 'make the html5 markup more semantic'
+git ci -m 'more cleanups: avoid braindead portability'
+git ci -m 'optimization'
 git ci -m 'package.json'
+git ci -m 'refactor'
+git ci -m 'refactoring'
+git ci -m 'still reproducing leak'
 git ci -m fix
+git ci -m optimize
 git clean -dxf .
 git clean -dxf ..
 git clean -dxf lib/docbook/5/
 git clean -dxf lib/presentations/qp/
 git co .
+git co ..
 git co master
+git com
 git dh
+git dh | gvim -
 git dhs
+git di master
 git i
+git init .
 git log --stat
 git log --stat | less
 git log -p
@@ -123,137 +238,301 @@ git log -p | gvim -
 git log -p | less
 git log .
 git log . | less
+git ls
 git ls-files t2 | grep wml | xargs touch
+git ls-files t2/humour/bits/facts/ | grep wml | xargs touch
 git push
 git push --tags
 git pushu
 git rebase master
+git reset .
 git s o
 git s u
+git show
+git st
 git stash
+git stash apply
+git stash drop
+git stash list
 git stash pop
 git tag -l
+gmake
+go get -u github.com/tdewolff/minify/cmd/minify
+grep -F found ../l4.txt
+gringotts
 gvim
+gvim $(__this_theme_source )
 gvim +mak
+gvim -S a.sess
 gvim -o .travis.yml /home/shlomif/Docs/homepage/homepage/trunk/.travis.yml
+gvim -o lib/hunspell/whitelist1.txt Tests/valid-html-tidy.t Tests/gmake-unit.t
+gvim -o src/js/fcs-validate.ts src/js/web-fc-solve-tests--fcs-validate.ts
+gvim -o y.txt lib/hunspell/whitelist1.txt
+gvim ../c-solver/black_hole_solver.c
+gvim ../scripts/multi_config_tests.pl
+gvim ../source/CMakeLists.txt
+gvim .appveyor.yml
+gvim .gen-ci.bash
 gvim .gitignore
 gvim .travis.yml
+gvim /home/shlomif/conf/trunk/shlomif-settings/home-bin-executables/bin/dnf-py-scan.bash
 gvim Changes
+gvim FAQ.mdwn
+gvim Makefile
+gvim README.asciidoc
+gvim README.md
+gvim Rakefile
 gvim TODO.quicktask
+gvim Tests/style-trailing-space.t
+gvim Tests/valid-html-tidy--5.t
+gvim Tests/valid-html-tidy.t
 gvim Tests/validate-html-using-vnu.py
+gvim bin/ci-generate
 gvim bin/gen-docbook-make-helpers.pl
+gvim bin/private/backup-extra-exclude-list-for-rsync.net.txt
+gvim bin/process.pl
+gvim bin/rebuild
+gvim bin/required-modules.yml
+gvim blocks.tt2
+gvim build.sh
 gvim dist.ini
+gvim gen-helpers
+gvim lib.c
+gvim lib/App/HTML/PostProc/Gezer.pm
+gvim lib/hunspell/whitelist1.txt
 gvim lib/make/main.mak
 gvim lib/rest-of-template.wml
+gvim rakefile
+gvim rpm-qa.txt
+gvim run-tests.pl
+gvim src/download.html.wml
+gvim src/js/solver-freecell.js
+gvim src/js/web-fc-solve-tests.ts
+gvim tslint.json
+gvimdiff dest/web-forums/index.html ../dest/web-forums/index.html +colorscheme" apprentice" +"exe \"normal \\<c-w>J\""
+gwenview
 hc
 hebrew-keymap.sh
 hexchat
 hg ci -m 'add more'
+hg clean --all
 hg pull
 hg push
 hg st
 hm
+htop
+i
 i=0; while bash SPLIT_FCC/by-depth/2/active/AAAAAAAAAAAAAAAARA==/driver.bash ; do let i++ ; done ; echo $i
+i=0; while bash SPLIT_FCC/by-depth/3/active/AAAAAAAAAAAAAAABFA==/driver.bash ; do let i++ ; done ; echo $i
 ifconfig -a
+k
 k5
 kate
+kill %1
+kmix
 kpat
+la
 ldd freecell-solver-multi-thread-solve
+less .tidyallrc
 less .travis.yml
+less LICENSE
+less dist.ini
+less l
+less lib/docbook/5/essays/foss-and-other-beasts-v3/all-in-one.xhtml
+li
 ls
 ls -l
+ls -lh
 ls -lrS
 ls -lrt
 ls -ltA
 ls -rt
+ls t
 lynx www.shlomifish.org
 m
+m clean
+m clean all
 make
+make -f C_arraytest.mak
+make -j4
 make -j4 fastrender
 make -j8
+make -j8  upload
+make -j8 upload_var
+make 2>&1 | gvim -
+make PROD=0 upload_beta
+make PROD=1
 make PROD=1 upload
+make PROD=1 upload_beta
+make VERBOSE=1 > m.txt 2>&1
+make check
 make clean
 make disttest
+make gen_divs
 make install
+make lib/docbook/5/essays/foss-and-other-beasts-v3/all-in-one.xhtml
+make oldconfig
 make package_source
 make test
 make upload
 make upload_beta
+make upload_var
+mcs -pkg:dotnet 630-v1.cs
 mcs -pkg:dotnet 630-v1.cs 2>&1 | less
 mgarepo up
 mi
 minicpan
+mkdir b
+mono ./630-v1.exe
+mr st
 n -m foo
+ninja -j1
+node test.js && echo succ
+nvim -- dbm_solver.h
+nvim -o e658log2par.txt 658/par2.bash
+nvim solve-more-7-gnupar.bash
+nvim-qt
+nvim-qt -- dbm_solver.h
 p --tags
 perl ../scripts/cmd-line-compiler compile && m && y
 perl ../scripts/multi_config_tests.pl ; n --msg 'fcs test'
+perl ../src/wml_test/run_test.pl
 perl 658/summarize.pl
 perl Makefile.PL
+perl Tests/spell-check--hebrew.t
+perl apply-solve-more-3-log.pl
+perl bin/rel-links-parse.pl $(find post-dest/ -name '*.html' -o -name '*.xhtml')
 perl bin/sort-check-spelling-file
+perl tests/installation/lib64/wml/exec/wml_p1_ipp -
 perl6 ../scripts/fuzz-build.p6 ; n -m clang
+perlbrew use perl-5.28.0
 perldoc Path::Tiny
 pi-make-microsoft-freecell-board -t 830910836 | ./fc-solve --load-config video-editing  -p -t -sam -sel -mi 200000
 pidgin
 ping www.google.com
+pkill gvim
 pm-suspend
+prettier --parser scss --arrow-parens always --tab-width 4 --trailing-comma all --write lib/sass/*.scss
+prettier --parser typescript --arrow-parens always --tab-width 4 --trailing-comma all --write src/js/fcs-validate.ts
 prompt fire
 prove Tests/validate-html-using-vnu.py 2>&1 | tee ~/f
+prove shlomif-settings/tests/*.t
+ps axo pid,cmd,etime | grep make
 pt
 pwd
+pypy3 -i 658/658_v1.py
 pypy3 654_v1.py
+pypy3 655_v1.py
+pypy3 657/657_v2.py
+pypy3 658/658_v1.py | tee -a n
+pypy3 658/658_v1.py| timestamper | tee -a e658-log0.txt
 pypy3 663_v1.py
+pypy3 665_v1.py | timestamper
+pypy3 euler-248-v1.py
 pypy3 sums_of_powers.py
+python build/build.py all
+python build/build.py jar
 python build/build.py test
+python3 -mpdb pysol.py
+python3 ../../source/t/t/several-iter-limits.py
+python3 654_v1.py
+python3 657/657_v1.py
+python3 658/658_v1.py
+python3 658/658_v1.py | tee -a 658log.txt
+python3 658/658_v1.py| timestamper | tee -a e658-log1.txt
+python3 662_v1.py
 python3 Tests/validate-html-using-vnu.py
+python3 pysol.py
 python3 setup.py build
 python3 setup.py test
 python3 setup.py test 2>&1 | gvim -
 python3 setup.py test 2>&1 | tee ~/inkscape-ext-tests.txt
 pythonsetup.py sdist
+qtb
+r '- New version'
 rake
 rake prettier
 rake test
 reboot
 rebuild
+rebuild 2>&1 | tee ~/o.txt ; n -m rebuild
+rebuild ; n -m rebuild
 regen
 rehash
 rej
+rgm i
+rgm perl6 e396_v1.p6
+rgm t
 rm *~
+rm -f offload/fcs_queue*
+rm -f t/verify-cache/*
+rm -f t/verify-cache/nht.sha
+rm -fr $TIDYALL_DATA_DIR
+rm -fr C*
 rm -fr CM*
 rm -fr RPMS/ SRPMS/ BUILD BUILDROOT/
 rm -fr SPLIT_FCC
+rm backup.tar
+rm f
+rm foo.txt
+rm lib/cache/STAMP.two
 rpmbuild --undefine=_disable_source_fetch -ba /home/shlomif/progs/Rpms/SPECS/lepton.spec
 run
+s
 sd
+sensors
+ssh hostgator
 ssh lap
+startup
 strip -s *
 sub
 sudo -i
+sudo dnf -y --refresh upgrade
 sudo iotop
+sudo urpmi --auto-select --auto --auto-update
 svn di
 svn st
 t
+tail ~/d.txt
 tidyall -a
 time ./a.out
+time ./e451.exe
+time ./mniip
+time make
+time pypy 630-v1.py
+time python3 650/650-v1.py
 time pythone-brob.py
 time rebuild
 tmux
 tmux a
 tmux ls
+touch lib/docbook/5/xml/*
 touch lib/template.wml
 touch lib/template.wml lib/template5.wml
+touch src/js/fcs-base-ui.ts
+touch t2/*.wml
+touch t2/humour/index.xhtml.wml
 touch t2/index.xhtml.wml
 ts
 u /home/shlomif/progs/Rpms/RPMS/noarch/task-shlomif-homesite-0.0.1-1.mga7.noarch.rpm
 uas --split-length
 ub
 uname -a
+unset DBUS_SESSION_BUS_ADDRESS
 unset HTML_VALID_VNU_JAR
 unset MAKEFLAGS
 up
 up $(dzil authordeps)
+uptime
 ur
 uu
+wc -l o.txt
 which quadp
+which wml
+xsetbg  ~/Download/Images/Women/hjZIPaw.jpg
 xsetbg  ~/Download/Images/Women/rLUd3uL.jpg
+xsetbg ~/Download/Images/Women/hjZIPaw.jpg
+xsltproc -o lib/docbook/5/indiv-nodes/case-for-drug-legalisation-v3/ --stringparam docmake.output.format xhtml --stringparam docmake.output.path_to_root ../../ --stringparam docmake.output.work_in_progress '' lib/sgml/shlomif-docbook/xsl-5-stylesheets/shlomif-essays-5-xhtml.xsl lib/docbook/5/xml/case-for-drug-legalisation-v3.xml
 xsltproc -v -o lib/docbook//indiv-nodes/case-for-drug-legalisation-v/ --stringparam docmake.output.format xhtml --stringparam docmake.output.path_to_root ../../ --stringparam docmake.output.work_in_progress '' lib/sgml/shlomif-docbook/xsl--stylesheets/shlomif-essays--xhtml.xsl lib/docbook//xml/case-for-drug-legalisation-v.xml
+zef install --force-install .
+zsh
