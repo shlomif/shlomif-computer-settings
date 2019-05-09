8e161b71e326cafbaacea7dd726fdc478e99eb20
Shlomi Fish <shlomif@shlomifish.org>
2019-05-09 20:12:28 +0300

    add zsh

--- shlomif-settings/bash-aliases/common_aliases.bash
+++ shlomif-settings/bash-aliases/common_aliases.bash
@@ -72,12 +72,42 @@ eo()
 alias q='dnf search'
 alias qv='q --source'
 
+__delegate_complete()
+{
+    local to="$1"
+    shift
+    local from="$1"
+    shift
+
+if test -n "$BASH_VERSION"
+then
+    complete -F "$to" "$from"
+elif test -n "$ZSH_VERSION"
+then
+    compdef "$from=$tp"
+fi
+}
+
 if test -n "$BASH_VERSION"
 then
     complete -F dnf u
     complete -F dnf e
     complete -F dnf q
     complete -F dnf qv
+elif test -n "$ZSH_VERSION"
+then
+    __complete_e() {
+        ((CURRENT++))
+        words=(sudo dnf remove "${words[@]:1}")
+        _normal
+    }
+    compdef __complete_e e
+    __complete_u() {
+        ((CURRENT++))
+        words=(dnf install "${words[@]:1}")
+        _normal
+    }
+    compdef __complete_u u
 fi
 
 alias hc='htop -s PERCENT_CPU'
