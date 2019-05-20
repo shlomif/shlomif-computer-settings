64dee80cad643e5a98f31310d1121c712fd135c6
Shlomi Fish <shlomif@shlomifish.org>
2019-05-20 17:31:34 +0300

    add a program to print fibonacci.
    See https://oeis.org/wiki/Fibonacci_numbers and
    https://en.wikipedia.org/wiki/Fibonacci_number .

---
+++ shlomif-settings/home-bin-executables/bin/fibonacci.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+
+def fib():
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x+y
+
+
+for x in fib():
+    print(x)
