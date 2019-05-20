71a7131c3d88255fa4711895411d73264f9279c6
Shlomi Fish <shlomif@shlomifish.org>
2019-05-20 15:32:37 +0300

    add cpython

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/cpython.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# cpython.sh
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+git hub clone https://github.com/shlomif/cpython
