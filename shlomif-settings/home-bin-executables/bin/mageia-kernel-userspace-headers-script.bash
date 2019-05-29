406be94f67caaf69bedc17cac5e601d28f054a26
Shlomi Fish <shlomif@shlomifish.org>
2019-05-29 13:45:37 +0300

    update.

--- shlomif-settings/home-bin-executables/bin/mageia-kernel-userspace-headers-script.bash
+++ shlomif-settings/home-bin-executables/bin/mageia-kernel-userspace-headers-script.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-kver="4.7.3"
-ever="1.mga6"
+kver="5.1.5"
+ever="1.mga7"
 
 kpath="kernel-userspace-headers-$kver.$ever"
 
 
 path="$HOME/kernel-userspace-headers"
 
-
+rm -f include/uapi/linux/Kbuild
 mkdir -p "$path"
 make INSTALL_HDR_PATH="$path" headers_install_all
 find "$path" -name ..install.cmd -exec rm -f {} \;
