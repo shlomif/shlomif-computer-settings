7f0bf71b6916549845bd00004ac11ed709cdb2dd
Shlomi Fish <shlomif@shlomifish.org>
2019-06-15 13:24:51 +0300

    add split file into buckets.

---
+++ shlomif-settings/home-bin-executables/bin/split-file-into-buckets.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+import sys
+
+from six import print_
+
+i = open(sys.argv[1], "rt")
+o = {}
+cnt = 0
+for line in i:
+    pref = line[:2]
+    if pref not in o:
+        o[pref] = open("parts/{}.txt".format(pref), "at")
+    o[pref].write(line[2:])
+    cnt += 1
+    if not cnt & 0xFFFF:
+        print_("Reached {}".format(cnt), file=sys.stderr)
