d5162c0d8bb5e2172a075786733b4e7981bcdbd8
Shlomi Fish <shlomif@shlomifish.org>
2019-06-28 22:54:59 +0300

    add a script/

---
+++ shlomif-settings/home-bin-executables/bin/trim-dup-github-backups.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+# trim-dup-github-backups.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+for i in *.git
+do
+    d=../../s/shlomif/"$i"
+    test -e "$d" && test -z "`diff -u -r "$d" "$i"`" && echo "$i"
+done
