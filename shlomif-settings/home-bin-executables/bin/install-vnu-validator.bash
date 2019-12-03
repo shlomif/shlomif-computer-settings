6392d5dc48e5dee206c362931fcbdb41f9a2652e
Shlomi Fish <shlomif@shlomifish.org>
2019-12-03 08:41:05 +0200

    install-vnu-validator.bash

---
+++ shlomif-settings/home-bin-executables/bin/install-vnu-validator.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# install-vnu-validator.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e -x
+base="$HOME/Download/unpack/net/www/"
+mkdir -p "$base"
+cd "$base"
+if ! test -d "validator"
+then
+    git clone 'https://github.com/validator/validator'
+fi
+cd "validator"
+export JAVA_HOME="/usr"
+python3 ./checker.py all
+python3 ./checker.py jar
