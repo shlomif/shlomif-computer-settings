7790494b349bd78d0864ed22916a898a857fde60
Shlomi Fish <shlomif@shlomifish.org>
2019-12-08 11:47:19 +0200

    flake8

--- shlomif-settings/home-bin-executables/bin/notifier-event-show-hook--pyqt5.py
+++ shlomif-settings/home-bin-executables/bin/notifier-event-show-hook--pyqt5.py
@@ -7,14 +7,15 @@ Author: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
 Date: 25 December 2018
 """
 
-import click
-
 import platform
-import os
 import sys
 
-from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import click
+
 import vlc
+
 
 class Player(QtWidgets.QMainWindow):
     """A simple Media Player using VLC and Qt
@@ -43,7 +44,7 @@ class Player(QtWidgets.QMainWindow):
         self.setCentralWidget(self.widget)
 
         # In this widget, the video will be drawn
-        if platform.system() == "Darwin": # for MacOS
+        if platform.system() == "Darwin":
             self.videoframe = QtWidgets.QMacCocoaViewContainer(0)
         else:
             self.videoframe = QtWidgets.QFrame()
@@ -154,11 +155,11 @@ class Player(QtWidgets.QMainWindow):
         # video would be displayed in it's own window). This is platform
         # specific, so we must give the ID of the QFrame (or similar object) to
         # vlc. Different platforms have different functions for this
-        if platform.system() == "Linux": # for Linux using the X Server
+        if platform.system() == "Linux":  # for Linux using the X Server
             self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
-        elif platform.system() == "Windows": # for Windows
+        elif platform.system() == "Windows":  # for Windows
             self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
-        elif platform.system() == "Darwin": # for MacOS
+        elif platform.system() == "Darwin":  # for MacOS
             self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
 
         self.play_pause()
@@ -171,11 +172,6 @@ class Player(QtWidgets.QMainWindow):
     def update_ui(self):
         """Updates the user interface"""
 
-        # Set the slider's position to its corresponding media position
-        # Note that the setValue function only takes values of type int,
-        # so we must first convert the corresponding media position.
-        media_pos = int(self.mediaplayer.get_position() * 1000)
-
         # No need to call this function if nothing is played
         if not self.mediaplayer.is_playing():
             self.timer.stop()
@@ -186,9 +182,13 @@ class Player(QtWidgets.QMainWindow):
             if not self.is_paused:
                 self.stop()
 
+
 @click.command()
-@click.option("--video", default="/home/shlomif/Music/mp3s/Jessie J - Domino-UJtB55MaoD0.webm", help="file to play")
-@click.option("-m", "--msg", default="Your task has finished - go check it out!", help="notification message")
+@click.option("--video", default="/home/shlomif/Music/mp3s/" +
+              "Jessie J - Domino-UJtB55MaoD0.webm", help="file to play")
+@click.option("-m", "--msg",
+              default="Your task has finished - go check it out!",
+              help="notification message")
 def main(video, msg):
     """Entry point for our simple vlc player
     """
@@ -197,6 +197,7 @@ def main(video, msg):
     player.show()
     player.resize(640, 480)
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
