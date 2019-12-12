42fddbfac022b61bdd076bf5f6f9de1bf6b3e738
Shlomi Fish <shlomif@shlomifish.org>
2019-12-12 11:06:28 +0200

    avoid clutter

--- shlomif-settings/Bash/Themes/themes/homepage/source.bash
+++ shlomif-settings/Bash/Themes/themes/homepage/source.bash
@@ -13,6 +13,11 @@ logs="$HOME/Download/homesite-logs"
 p4n="$trunk/lib/presentations/qp/perl-for-newbies"
 remote_repo="$(_shlomif_github "shlomi-fish-homepage")"
 export QUAD_PRES_CACHE_DIR=~/Arcs/temp/quad-pres-cache
+export PERL_INLINE_DIRECTORY="$TMPDIR/shlomif-hp-Inline"
+if ! test -d "$PERL_INLINE_DIRECTORY"
+then
+    mkdir -p "$PERL_INLINE_DIRECTORY"
+fi
 pristine_copy=~/Backup/Arcs/post-dest/post-incs
 pristine_copy_reduced=~/Backup/Arcs/post-dest/post-incs-reduced
 this_copy_reduced=~/Backup/Arcs/post-dest/this-incs-reduced
