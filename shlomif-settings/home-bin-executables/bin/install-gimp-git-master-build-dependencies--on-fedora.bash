43ccb43049aead451b2327021320dd3edff1ab9f
Shlomi Fish <shlomif@shlomifish.org>
2020-01-03 19:51:21 +0200

    add dep

--- shlomif-settings/home-bin-executables/bin/install-gimp-git-master-build-dependencies--on-fedora.bash
+++ shlomif-settings/home-bin-executables/bin/install-gimp-git-master-build-dependencies--on-fedora.bash
@@ -7,6 +7,7 @@
 #
 
 sudo dnf -y builddep gimp
-sudo dnf -y install automake gjs-devel gobject-introspection-devel libappstream-glib-devel libarchive-devel libgjs-devel meson
+sudo dnf -y install automake gjs-devel gobject-introspection-devel libappstream-glib-devel libarchive-devel libgjs-devel luajit-devel meson
+cd ~
 ln -sf /usr/bin/automake-1.16 bin/automake-1.13
 ln -sf /usr/bin/aclocal-1.16 bin/aclocal-1.13
