678c79f764429263d38567978e62bac7167cef25
Shlomi Fish <shlomif@shlomifish.org>
2020-01-14 11:44:38 +0200

    use at your own risk.

---
+++ shlomif-settings/build-scripts/build/bitkeeper-bk-report.bash
@@ -0,0 +1,45 @@
+#! /bin/bash
+#
+# bk-report.bash
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+build()
+{
+(
+set -e -x
+
+export CPATH=/usr/include/tomcrypt
+ver='7.3.3'
+cd ~/Download/unpack/to-del
+if test -e bk-bin
+then
+    chmod -R 777 bk-bin
+fi
+rm -fr bk-"$ver" bk-bin
+tar -xvf ~/Download/Arcs/bk-"$ver".src.tar.gz
+cd bk-"$ver"
+cd src
+make -j4 g
+make image
+) 2>&1 | tee ~/bk-report.txt
+}
+
+use()
+{
+    (
+    set -e -x
+    export PATH="$PATH:$HOME/Download/unpack/to-del/bk-bin"
+    bk version
+    if test -e dev
+    then
+        rm -fr dev
+    fi
+    uname -a
+    # valgrind bk clone bk://bkbits.net/u/bk/dev dev
+    bk clone bk://bkbits.net/u/bk/dev dev
+) 2>&1 | tee -a ~/bk-report.txt
+}
+
