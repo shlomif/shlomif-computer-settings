f1edbac6dfa9eb7fa43325effe0e9cfd86549c2d
Shlomi Fish <shlomif@shlomifish.org>
2020-01-28 11:09:14 +0200

    add a "source me.bash" script.

---
+++ shlomif-settings/home-bin-executables/bin/bleadperl-run-cpan-tests.bash
@@ -0,0 +1,4 @@
+( unset PERL5LIB PERL_LOCAL_LIB_ROOT PERL_LOCAL_LIB_ROOT PERL_MB_OPT PERL_MM_OPT ;
+~/apps/perl/bleadperl/bin/cpan5.31.9 CPANPLUS &&
+~/apps/perl/bleadperl/bin/cpanp -i Task::BeLike::SHLOMIF
+)
