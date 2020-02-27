3fa45ee6e975a9080be3b03c36968e3dac437022
Shlomi Fish <shlomif@shlomifish.org>
2020-02-27 14:12:07 +0200

    add mpv vcs .

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/mpv.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# mpv.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+set -e -x
+git clone https://github.com/mpv-player/mpv
