0b874a387ecd93172298f8e0cb7cc21b74c7b9bf
Shlomi Fish <shlomif@shlomifish.org>
2020-03-01 19:30:45 +0200

    add sum_walker.

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/sum_walker.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# sum_walker.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+git clone 'https://github.com/shlomif/sum_walker'
