bfd87867f5a7cac1c779c2147d348a2ec3bcf0e0
Shlomi Fish <shlomif@shlomifish.org>
2020-03-11 14:19:30 +0200

    add zsh build+clone scripts.

---
+++ shlomif-settings/build-scripts/build/zsh.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# zsh.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+./configure --prefix="$HOME/apps/to-del-zsh" "$@"
---
+++ shlomif-settings/vcs-checkout-scripts/scripts/zsh.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# zsh.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+git clone https://git.code.sf.net/p/zsh/code zsh-code
