61b1b17bcefc91ce50cbc8f956a6759d2adbf165
Shlomi Fish <shlomif@shlomifish.org>
2020-03-11 14:18:36 +0200

    install/build perl byacc.

--- shlomif-settings/Bash/Themes/themes/wml/itself/source.bash
+++ shlomif-settings/Bash/Themes/themes/wml/itself/source.bash
@@ -12,6 +12,11 @@ src="$trunk/src"
 # this="$trunk/src/wml_backend"
 b="$trunk/B"
 this="$b"
+pbyacc_base="perl-byacc"
+pbyacc_ver="2.0"
+pbyacc_basever="$pbyacc_base-$pbyacc_ver.orig"
+pbyacc_tarball_bn="${pbyacc_base}_$pbyacc_ver.orig.tar.bz2"
+pbyacc_dir="$core/$pbyacc_basever"
 
 remote_repo="$(_thewml_github "website-meta-language")"
 cd "$this"
@@ -72,8 +77,25 @@ c()
     cd "$src"
 }
 
+PATH="$HOME/apps/test/wml/bin/:$PATH:$pbyacc_dir"
+
+if ! which pbyacc
+then
+    pushd "$(pwd)"
+    cd "$core"
+    deburl="http://deb.debian.org/debian/pool/main/p/$pbyacc_base"
+    wget -c "$deburl/$pbyacc_tarball_bn"
+    pbyacc_debian_tarball_bn="perl-byacc_2.0-8.debian.tar.xz"
+    wget -c "$deburl/$pbyacc_debian_tarball_bn"
+    tar -xvf "$pbyacc_tarball_bn"
+    tar -xvf "$pbyacc_debian_tarball_bn"
+    cd "$pbyacc_dir"
+    patch -p1 < "../debian/patches/01-legacy.patch"
+    make
+    popd
+    rehash > /dev/null 2>&1
+fi
 proj_name='wml'
-export WML_TEST_BUILD=1 WML_TEST_TIDY=1
+export WML_TEST_BUILD=1 WML_TEST_TIDY=1 HARNESS_OPTIONS="j9:c"
 export TIDYALL_DATA_DIR="$HOME/Backup/Arcs/wml-itself-tidyall.d"
 
-PATH="$HOME/apps/test/wml/bin/:$PATH"
