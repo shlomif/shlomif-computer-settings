f1fdbe947d95b642190f9c62b0ca77ed25615412
Shlomi Fish <shlomif@shlomifish.org>
2020-03-30 15:24:31 +0300

    add shell var

--- shlomif-settings/Bash/Themes/themes/gimp/core/source.bash
+++ shlomif-settings/Bash/Themes/themes/gimp/core/source.bash
@@ -7,6 +7,7 @@ trunk="$git_base/gimp"
 this="$trunk"
 remote_repo='git://git.gnome.org/gimp'
 proj_name="gimp"
+c_src="$this"
 
 export PKG_CONFIG_PATH="$HOME/apps/graphics/gegl/lib/pkgconfig:$HOME/apps/graphics/babl/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$HOME/apps/graphics/gegl/lib:$HOME/apps/graphics/babl/lib:$LD_LIBRARY_PATH"
