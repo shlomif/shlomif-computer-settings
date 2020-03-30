2adbc8230169a04b0f6cd0b895a0dfc1e95e6914
Shlomi Fish <shlomif@shlomifish.org>
2020-03-30 15:20:31 +0300

    change to a better prefix.

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -11,5 +11,5 @@ if test "$NO_NINJA" = 1 ; then
 fi
 
 prefix="$HOME/apps/graphics/inkscape-trunk"
-prefix="/opt/vlc-3.0/inkscape-trunk"
-cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON -DWITH_JEMALLOC=ON -DWITH_GRAPHICS_MAGICK=ON -DWITH_OPENMP=ON -DWITH_LIBCDR=ON -DWITH_GTKSPELL=ON $flags "$@"
+prefix="/opt/`whoami`/inkscape-trunk"
+cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER=ON -DENABLE_POPPLER_CAIRO=ON -DWITH_DBUS=ON -DWITH_JEMALLOC=ON -DWITH_GRAPHICS_MAGICK=ON -DWITH_OPENMP=ON -DWITH_LIBCDR=ON -DWITH_GTKSPELL=ON  -DBUILD_TESTING=ON $flags "$@"
