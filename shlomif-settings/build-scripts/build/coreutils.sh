e721ca72da091e3a116f8e66c0fe98b7b886d28b
Shlomi Fish <shlomif@shlomifish.org>
2020-03-30 15:19:29 +0300

    optimize

--- shlomif-settings/build-scripts/build/coreutils.sh
+++ shlomif-settings/build-scripts/build/coreutils.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+# export CC="/usr/bin/gcc"
+export CFLAGS="-O3 -march=native -flto -fwhole-program -fomit-frame-pointer"
+export CFLAGS="-O3 -march=native -fwhole-program -fomit-frame-pointer"
+export CFLAGS="-O3 -march=native"
 ./configure --prefix="$HOME/apps/to-del-coreutils"
