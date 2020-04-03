366145172155dd472bcf9b34eda47d8264c32ad1
Shlomi Fish <shlomif@shlomifish.org>
2020-04-03 17:35:02 +0300

    add vcs-scripts

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/redis.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# redis.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+git clone https://github.com/antirez/redis/
---
+++ shlomif-settings/vcs-checkout-scripts/scripts/talking-arch.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/alex19EP/talking-arch.git
+# git clone git://projects.archlinux.org/archiso.git
