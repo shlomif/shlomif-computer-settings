0c000ae24d5ad4cc2380d458493504ad121a046d
Shlomi Fish <shlomif@shlomifish.org>
2020-04-05 22:09:17 +0300

    add webdiff command

---
+++ shlomif-settings/home-bin-executables/bin/git-hubdiff.pl
@@ -0,0 +1,30 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use 5.016;
+
+#
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+my @remote = `git remote -v`;
+
+#say $remote;
+#
+my ( $host, $user, $repo ) = (
+    map {
+m#\Aorigin\s+(?:[^\@]*\@|https?://)(?P<host>[^:/]+)[:/](?P<user>[^/]+)/(?P<repo>[a-zA-Z0-9\-_]+)#
+            ? @+{qw/ host user repo /}
+            : ()
+    } @remote
+) or die "no origin remote";
+
+my $orig_b = shift @ARGV;
+my $new_b  = shift @ARGV;
+
+my $url = qq#https://${host}/${user}/${repo}/compare/${orig_b}..${new_b}#;
+
+say $url;
