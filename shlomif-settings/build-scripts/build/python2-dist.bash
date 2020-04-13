f31152efa04983c31791a6c00eea52c4738fdcba
Shlomi Fish <shlomif@shlomifish.org>
2020-04-13 08:00:39 +0300

    add python2 build script.

---
+++ shlomif-settings/build-scripts/build/python2-dist.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+./configure --prefix="$HOME/apps/python2" --with-threads \
+                --with-system-expat \
+                --with-system-ffi \
+                --enable-shared \
+
