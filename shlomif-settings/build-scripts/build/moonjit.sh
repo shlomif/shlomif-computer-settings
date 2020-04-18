78d08fc35f868cb2ef7f8a3f9b0d648cae827bbe
Shlomi Fish <shlomif@shlomifish.org>
2020-04-18 16:30:18 +0300

    add moonjit

---
+++ shlomif-settings/build-scripts/build/moonjit.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# moonjit.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+make PREFIX="$HOME/apps/to-del-moonjit" all install
