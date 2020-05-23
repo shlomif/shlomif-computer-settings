854dda659594c482b0623ee5dd6011331c3b1c3a
Shlomi Fish <shlomif@shlomifish.org>
2020-05-23 08:40:45 +0300

    add gnu parallel

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/gnu-parallel.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# gnu-parallel.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+# git clone shlomifish@git.savannah.gnu.org:/srv/git/parallel.git
+git clone https://git.savannah.gnu.org/git/parallel.git
