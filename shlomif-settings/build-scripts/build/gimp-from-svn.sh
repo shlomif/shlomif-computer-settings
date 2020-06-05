87a031e4e665a9a26c8e49718cac9054205c32a4
Shlomi Fish <shlomif@shlomifish.org>
2020-06-05 16:01:03 +0300

    disable jpeg2000

--- shlomif-settings/build-scripts/build/gimp-from-svn.sh
+++ shlomif-settings/build-scripts/build/gimp-from-svn.sh
@@ -4,6 +4,6 @@ export PATH="$gegl_p/bin:$PATH"
 export PKG_CONFIG_PATH="$gegl_p/lib/pkgconfig:$HOME/apps/graphics/libmypaint/lib/pkgconfig:$HOME/apps/graphics/babl/lib/pkgconfig:$PKG_CONFIG_PATH"
 CFLAGS="-g" \
     ./configure --prefix="$HOME"/apps/gimp-devel --enable-maintainer-mode \
-        --with-jpeg2000
+        # --with-jpeg2000
 
 # --enable-gtk-doc \
