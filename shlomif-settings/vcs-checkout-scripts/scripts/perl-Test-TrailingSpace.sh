c3c2d3b75026f1ca853f806ca0b39aeb86afcbeb
Shlomi Fish <shlomif@shlomifish.org>
2020-06-05 16:00:07 +0300

    add TrailingSpace.

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/perl-Test-TrailingSpace.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# perl-Test-TrailingSpace.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone 'https://github.com/shlomif/perl-test-trailingspace' 'perl-Test-TrailingSpace'
