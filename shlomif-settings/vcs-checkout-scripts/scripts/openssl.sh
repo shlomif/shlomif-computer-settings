2ef81170ac46839e33e9cfd804609da87dda2ea3
Shlomi Fish <shlomif@shlomifish.org>
2020-06-08 17:45:54 +0300

    add openssl

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/openssl.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# openssl.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+git clone "https://github.com/openssl/openssl"
