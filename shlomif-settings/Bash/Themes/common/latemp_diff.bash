90e9a88610bb3b9297531f6e6ccbd0a3e755fdc9
Shlomi Fish <shlomif@shlomifish.org>
2020-06-16 11:50:45 +0300

    add sanity checks.

--- shlomif-settings/Bash/Themes/common/latemp_diff.bash
+++ shlomif-settings/Bash/Themes/common/latemp_diff.bash
@@ -1,5 +1,11 @@
+
 ba()
 {
+    if test -z "$pristine_copy"
+    then
+        echo "pristine_copy must be set"
+        return
+    fi
     mkdir -p "$pristine_copy"
     rm -fr "$pristine_copy"
     rsync -a --exclude '**/MathJax/**' "$_post_dest/" "$pristine_copy"
@@ -7,6 +13,11 @@ ba()
 
 did()
 {
+    if test -z "$pristine_copy"
+    then
+        echo "pristine_copy must be set"
+        return
+    fi
     bash "$latemp_trunk/bin/reduce-differences-in-diff-dash-r.bash" "$pristine_copy" "$pristine_copy_reduced"
     bash "$latemp_trunk/bin/reduce-differences-in-diff-dash-r.bash" "$_post_dest" "$this_copy_reduced"
     diff -u -r "$pristine_copy_reduced" "$this_copy_reduced" | gvim -
--- shlomif-settings/Bash/Themes/themes/vim/begin/source.bash
+++ shlomif-settings/Bash/Themes/themes/vim/begin/source.bash
@@ -11,6 +11,11 @@ latemp_trunk="$src"
 remote_repo="$(_shlomif_github "vim-begin")"
 
 load_common latemp
+pristine_copy=~/Backup/Arcs/sites--vim-begin-post-dest/post-incs
+pristine_copy_reduced=~/Backup/Arcs/sites--vim-begin-post-dest/post-incs-reduced
+this_copy_reduced=~/Backup/Arcs/sites--vim-begin-post-dest/this-incs-reduced
+_post_dest="dest"
+load_common latemp_diff
 
 cd "$this"
 
