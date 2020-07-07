93915039e220097fe1293eacb8fa731d0f4f63a7
Shlomi Fish <shlomif@shlomifish.org>
2020-07-07 17:54:09 +0300

    add eyeofgnome scripts

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/eog.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#
+# gdk-pixbuf.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+git clone https://gitlab.gnome.org/GNOME/eog.git
