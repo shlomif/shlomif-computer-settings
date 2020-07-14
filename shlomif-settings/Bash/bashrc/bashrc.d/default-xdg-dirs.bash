4aef52779a93ac7438c0e9d1e08d1f6d8ac0b12f
Shlomi Fish <shlomif@shlomifish.org>
2020-07-14 22:03:34 +0300

    add default env var

---
+++ shlomif-settings/Bash/bashrc/bashrc.d/default-xdg-dirs.bash
@@ -0,0 +1,8 @@
+# default-xdg-dirs.bash
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the MIT license.
+#
+
+# See: https://bugs.mageia.org/show_bug.cgi?id=26948 .
+export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
