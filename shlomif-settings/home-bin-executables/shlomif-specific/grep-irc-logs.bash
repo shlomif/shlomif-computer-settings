a8e2f250ca69907d51cb7853e2b69fe214d083cf
Shlomi Fish <shlomif@shlomifish.org>
2020-07-18 21:15:15 +0300

    sort by relevance

--- shlomif-settings/home-bin-executables/shlomif-specific/grep-irc-logs.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/grep-irc-logs.bash
@@ -6,11 +6,16 @@
 chan_pat="$1"
 shift
 
-ls ~/Backup/old-dot-files/dot-xchat2/xchatlogs/*.log \
+# We use for instead of ls here because ls sorts the absolute paths:
+(
+for fn in ~/.local/share/hexchat/logs/*.log \
+   ~/.local/share/hexchat/logs/*/*.log \
+   ~/Backup/old-dot-files/dot-xchat2/xchatlogs/*.log \
    ~/Backup/old-dot-files/dot-xchat2/__keep-xchatlogs/*.log \
    ~/Backup/old-dot-files/dot-xchat2/saved-xchatlogs/*.log \
-   ~/.local/share/hexchat/logs/*.log \
-   ~/.local/share/hexchat/logs/*/*.log \
    ~/Backup/laptop/2017-04-04/disk-fs"$HOME"/.config/hexchat/logs/*/*.log \
    ~/Backup/laptop/2012-12-22/disk-fs/extra-data/unpack-for-grep/.xchat2/xchatlogs/*.log \
+; do
+    test -e "$fn" && echo "$fn"
+done) \
    | grep -iP "$chan_pat"
