928077f7652ac7fef5e68c3022e9d2ecea65553b
Shlomi Fish <shlomif@shlomifish.org>
2020-08-16 09:55:26 +0300

    revise templates.
    Grammar / best practices / etc.

---
+++ shlomif-settings/vim-conf/conf-Vim/templates/=template=.bash
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+#
+# %FFILE%
+# Copyright (C) %YEAR% %USER% < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the %LICENSE% license.
+#
+
+
+%HERE%
---
+++ shlomif-settings/vim-conf/conf-Vim/templates/=template=.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% %USER% < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the %LICENSE% license.
+
+"""
+%HERE%
+"""
