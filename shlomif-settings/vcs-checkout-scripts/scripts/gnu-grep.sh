94c0b05633474c2a69b0f42e8213f98aa9f528ca
Shlomi Fish <shlomif@shlomifish.org>
2020-08-18 16:45:02 +0300

    clone gnu-grep.

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/gnu-grep.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# gnu-grep.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone 'https://git.savannah.gnu.org/git/grep.git'
