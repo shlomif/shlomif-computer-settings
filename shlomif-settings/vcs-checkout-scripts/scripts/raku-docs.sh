36c541471fbe8f8211f161bbed92c325a234875d
Shlomi Fish <shlomif@shlomifish.org>
2020-08-23 16:48:54 +0300

    add raku docs vcs

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/raku-docs.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#
+# raku-docs.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+git hub clone 'https://github.com/shlomif/doc'
