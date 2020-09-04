9041d4a468d2acfa70811c3680536e3228bc0f44
Shlomi Fish <shlomif@shlomifish.org>
2020-09-04 10:31:30 +0300

    ohmyzsh

--- shlomif-settings/Bash/bashrc/zshrc.d/zoverride.zsh
+++ shlomif-settings/Bash/bashrc/zshrc.d/zoverride.zsh
@@ -1,8 +1,11 @@
 # oh-my-zsh booboo
 alias l='ls'
 unset LESS
-unalias g
-# For https://en.wikipedia.org/wiki/GraphicsMagick
-unalias gm
+if test "$(hostname)" != "telaviv1.shlomifish.org"
+then
+    unalias g
+    # For https://en.wikipedia.org/wiki/GraphicsMagick
+    unalias gm
+fi
 # set **.c to work as well as **/*.c
 setopt GLOB_STAR_SHORT
