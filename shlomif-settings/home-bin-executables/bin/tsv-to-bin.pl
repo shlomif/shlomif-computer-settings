431c6bb6580d08eb4e3a2ab19f5708bb9e94e1ce
Shlomi Fish <shlomif@shlomifish.org>
2020-09-04 12:28:17 +0300

    add tsv2bin: it makes 7z compression faster and compacter

---
+++ shlomif-settings/home-bin-executables/bin/tsv-to-bin.pl
@@ -0,0 +1,14 @@
+#!perl -na
+use strict;
+use warnings;
+use autodie;
+
+use vars qw/ $cnt $fmt /;
+
+BEGIN
+{
+    $cnt = $ENV{NUM_FIELDS} // 2;
+    $fmt = "q" x $cnt;
+}
+die if @F != $cnt;
+print pack( $fmt, @F );
