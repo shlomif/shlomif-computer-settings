db76162e9c0c431706a2a0f96800c84b04c60324
Shlomi Fish <shlomif@shlomifish.org>
2020-10-04 14:29:23 +0300

    add yui

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/yui3-js-library.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# yui3-js-library.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone https://github.com/yui/yui3
