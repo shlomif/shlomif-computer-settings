dbead9388afbba24062a894b8789595d73e50111
Shlomi Fish <shlomif@shlomifish.org>
2020-10-06 14:12:48 +0300

    add more html snippets

--- shlomif-settings/build-scripts/build/neovim.sh
+++ shlomif-settings/build-scripts/build/neovim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # cmake -DCMAKE_INSTALL_PREFIX="$HOME/apps/neovim" "$1"
 # echo "You should really be using the bootstrapping makefile" 1>&2
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/apps/neovim"
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/apps/neovim"
