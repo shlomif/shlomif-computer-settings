357ae011c7e6c0025ff2567afd4d63018f87c5ed
Shlomi Fish <shlomif@shlomifish.org>
2020-10-17 10:28:01 +0300

    add primesieve-python

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/primesieve-python.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#
+# primesieve.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+gh repo clone https://github.com/shlomif/primesieve-python
