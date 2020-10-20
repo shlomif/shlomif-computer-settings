9cc8d7535032b02bfdd0ae66c95340c4a5cc89ff
Shlomi Fish <shlomif@shlomifish.org>
2020-10-20 09:57:26 +0300

    add a vcs script.

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/perl-Graph-Easy-As_svg.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# perl-Graph-Easy-As_svg.sh
+# Copyright (C) 2020 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone "https://github.com/shlomif/Graph-Easy-As_svg"
