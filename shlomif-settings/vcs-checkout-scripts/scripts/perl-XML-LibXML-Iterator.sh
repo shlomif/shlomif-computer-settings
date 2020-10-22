461d9fe75bb34ce2f605e9aa2090ec8d9065ae3e
Shlomi Fish <shlomif@shlomifish.org>
2020-10-22 23:22:02 +0300

    add a vcs script perl-XML-LibXML-Iterator

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/perl-XML-LibXML-Iterator.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# perl-XML-LibXML-Iterator.sh
+# Copyright (C) 2020 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone "https://github.com/shlomif/xml-libxml-iterator"
