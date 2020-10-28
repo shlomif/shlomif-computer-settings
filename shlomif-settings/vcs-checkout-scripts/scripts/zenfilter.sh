73e4aeaf5d9daa03ae6a35d79cf8eedb4242ca44
Shlomi Fish <shlomif@shlomifish.org>
2020-10-28 18:46:36 +0200

    add a vcs script zenfilter

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/zenfilter.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#
+# sum_walker.sh
+# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+git clone 'https://github.com/shlomif/zenfilter'
