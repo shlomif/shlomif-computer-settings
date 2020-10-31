7d8adfdeed3c4fe40e72078c24dfa390f6f58c29
Shlomi Fish <shlomif@shlomifish.org>
2020-10-31 15:47:27 +0200

    add more and sort

--- shlomif-settings/home-bin-executables/shlomif-specific/cleanup-mageia-packages.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/cleanup-mageia-packages.bash
@@ -6,18 +6,20 @@
 # Distributed under the terms of the MIT license.
 #
 
-e task-shlomif-homesite
-e task-shlomif-fizzbuzz
-e perl-Test-XML
+e $(rpm -qa 'lib64opencv*')
+e cvs-fast-export
+e git-arch
+e git-cvs
 e glibc-devel
+e gnome-control-center
+e netprofile
+e perl-Mojolicious
+e perl-Test-XML
 e python2-numpy
 e ruby
 e ruby-irb
-e wml
-e $(rpm -qa 'lib64opencv*')
-e gnome-control-center
-e git-core
-e netprofile
 e sselp
-e perl-Mojolicious
+e task-shlomif-fizzbuzz
+e task-shlomif-homesite
+e wml
 eo
