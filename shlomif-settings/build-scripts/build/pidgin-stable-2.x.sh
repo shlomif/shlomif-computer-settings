809c8341cfb53945722db5afd00d159b78a34a80
Shlomi Fish <shlomif@shlomifish.org>
2020-11-23 10:22:12 +0200

    add pidgin build

--- shlomif-settings/build-scripts/build/pidgin-stable-2.x.sh
+++ shlomif-settings/build-scripts/build/pidgin-stable-2.x.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pref="$HOME/apps/pidgin-beta"
-./configure --prefix="$pref" --disable-vv --disable-meanwhile --disable-nm --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
+pref="$HOME/apps/pidgin-2.x"
+./configure --prefix="$pref" # --disable-vv --disable-meanwhile --disable-nm --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
