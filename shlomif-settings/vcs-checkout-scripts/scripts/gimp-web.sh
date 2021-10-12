3ad17226fc4a85c950e9e0dd4f297da37d0e1942
Shlomi Fish <shlomif@shlomifish.org>
2021-10-12 19:11:37 +0300

    add gimp-web

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/gimp-web.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#
+# Copyright (C) 2021 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+git clone https://gitlab.gnome.org/Infrastructure/gimp-web
