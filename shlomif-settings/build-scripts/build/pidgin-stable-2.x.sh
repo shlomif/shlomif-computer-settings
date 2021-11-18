c0d68dca41543bee3a5c6385fe5a60066e34210f
Shlomi Fish <shlomif@shlomifish.org>
2021-11-18 19:25:30 +0200

    add a configure flag

--- shlomif-settings/build-scripts/build/pidgin-stable-2.x.sh
+++ shlomif-settings/build-scripts/build/pidgin-stable-2.x.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pref="$HOME/apps/pidgin-2.x"
 # ./configure --prefix="$pref" # --disable-vv --disable-meanwhile --disable-nm --with-gconf-schema-file-dir="$pref/gconf/" --disable-schemas-install
-./configure --prefix="$pref" --disable-vv --disable-gstreamer
+./configure --prefix="$pref" --disable-vv --disable-gstreamer --disable-gevolution
