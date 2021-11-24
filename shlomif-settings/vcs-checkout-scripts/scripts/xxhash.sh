806eddeaabf05910151ca614dbb0b01a65f99225
Shlomi Fish <shlomif@shlomifish.org>
2021-11-24 11:11:22 +0200

    add

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/xxhash.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# xxhash.sh
+# Copyright (C) 2021 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone https://github.com/Cyan4973/xxHash/
