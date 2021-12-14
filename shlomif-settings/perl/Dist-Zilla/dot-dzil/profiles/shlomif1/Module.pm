e13bcfed0b1bfa56cb0a5e6646934d8e3d51e29b
Shlomi Fish <shlomif@shlomifish.org>
2021-12-14 09:52:03 +0200

    dzil updates

--- shlomif-settings/perl/Dist-Zilla/dot-dzil/profiles/shlomif1/Module.pm
+++ shlomif-settings/perl/Dist-Zilla/dot-dzil/profiles/shlomif1/Module.pm
@@ -1,6 +1,26 @@
 package {{$name}};
 
+use 5.014;
 use strict;
 use warnings;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
+
