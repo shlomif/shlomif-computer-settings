941a99326da4c03915c56de69b1344827b8a6df9
Shlomi Fish <shlomif@shlomifish.org>
2022-05-10 10:12:41 +0300

    more settings

---
+++ shlomif-settings/build-scripts/build/lynx.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# lynx.sh
+# Copyright (C) 2022 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+./configure --prefix="$HOME/apps/lynx-beta" --with-ssl
