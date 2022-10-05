e945d36386fd82d39b3fdecc3134ebaa0e19b26c
Shlomi Fish <shlomif@shlomifish.org>
2022-10-05 03:45:08 +0300

    add a script

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/cmocka.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# cmocka.sh
+# Copyright (C) 2022 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone https://gitlab.com/cmocka/cmocka
