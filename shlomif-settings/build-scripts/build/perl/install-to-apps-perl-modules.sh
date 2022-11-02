401301000bbfb8cd7404766b7ea0d03efd81be27
Shlomi Fish <shlomif@shlomifish.org>
2022-11-02 17:04:14 +0200

    add licence

--- shlomif-settings/build-scripts/build/perl/install-to-apps-perl-modules.sh
+++ shlomif-settings/build-scripts/build/perl/install-to-apps-perl-modules.sh
@@ -1,25 +1,47 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2022 Shlomi Fish ( https://www.shlomifish.org/ )
+# Author: Shlomi Fish ( https://www.shlomifish.org/ )
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+set -e -x
 b_pl="Build.PL"
 prefix="$HOME/apps/perl/modules"
 
-if test -e "$b_pl" ; then
+if test -e "$b_pl"
+then
     (
         unset PERL_MM_OPT
         unset MODULEBUILDRC
         args="--prefix=$prefix"
-        perl "$b_pl" $args && \
-            ./Build build $args && \
-            ./Build test $args && \
-            ./Build install $args
+        perl "$b_pl" $args
+        ./Build build $args
+        ./Build test $args
+        ./Build install $args
     )
 else
     (
-        # perl Makefile.PL PREFIX="$prefix" INSTALLSITEBIN="\$(SITEPREFIX)/bin" INSTALLSITESCRIPT="\$(SITEPREFIX)/bin" &&
-        # perl Makefile.PL PREFIX="$prefix" && \
-        perl Makefile.PL && \
-        make && \
-        make disttest && \
+        # perl Makefile.PL PREFIX="$prefix" INSTALLSITEBIN="\$(SITEPREFIX)/bin" INSTALLSITESCRIPT="\$(SITEPREFIX)/bin"
+        # perl Makefile.PL PREFIX="$prefix"
+        perl Makefile.PL
+        make
+        make disttest
         make install
     )
 fi
