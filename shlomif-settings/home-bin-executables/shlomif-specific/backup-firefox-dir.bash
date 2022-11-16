f5802fadb1d67fb025c4268239093848c852b81c
Shlomi Fish <shlomif@shlomifish.org>
2022-11-16 13:38:18 +0200

    Optimize the speed.
    See https://en.wikibooks.org/wiki/Optimizing_Code_for_Speed . There may have
    been some refactorings too.

--- shlomif-settings/home-bin-executables/shlomif-specific/backup-firefox-dir.bash
+++ shlomif-settings/home-bin-executables/shlomif-specific/backup-firefox-dir.bash
@@ -2,5 +2,5 @@
 cd "$HOME"
 d="$HOME/Backup/firefox-config/old-configs/"
 mkdir -p "$d"
-fn="$d/$(date +"%Y-%m-%d-%H").tar.xz"
+fn="$d/$(date +"%Y-%m-%d-%H").tar.zst"
 nice -19 tar -caf "$fn" .mozilla 2>/dev/null
