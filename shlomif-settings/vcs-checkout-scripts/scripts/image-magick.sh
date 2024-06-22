baf738c6e95ad387b6c3f17a8e42684ecc460b02
Shlomi Fish <shlomif@shlomifish.org>
2024-06-22 16:14:22 +0300

    add/revise a vcs script

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/image-magick.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# image-magick.sh
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+git clone https://github.com/ImageMagick/ImageMagick
