e827b8b25e060fee66bebb44e08d9401e99d176e
Shlomi Fish <shlomif@shlomifish.org>
2024-06-25 14:23:00 +0300

    made the update-fedora-kernel script more robust

--- shlomif-settings/home-bin-executables/bin/update-fedora-kernel-in-slash-boot.pl
+++ shlomif-settings/home-bin-executables/bin/update-fedora-kernel-in-slash-boot.pl
@@ -14,17 +14,43 @@ use autodie;
 
 use Path::Tiny qw/ path tempdir tempfile cwd /;
 
+STDOUT->autoflush(1);
+
 chdir( my $dir = path("/boot") );
 
+my @actions;
 foreach my $base ( 'vmlinuz', 'initramfs', )
 {
-    my ($first) = grep { -l $_ and !-e readlink($_) }
-        map { path( $base . ".old$_" ) } 1 .. 5;
-    die "no link found at base=$base" if !defined($first);
-    path($base)->move($first);
+    my @options = grep { -l $_ }
+        ( path($base), ( map { path( $base . ".old$_" ) } 1 .. 5 ) );
+    my (@valid) = grep { -e readlink($_) } @options;
+    print "valid = @valid\n";
+    if ( not @valid )
+    {
+        die "no link found at base=$base";
+    }
+
+    # path($base)->move($first);
     my @targets = sort { $a->[1] cmp $b->[1] } map {
         [ $_, scalar( $_->basename =~ s#([0-9]+)#sprintf('%09d', $1)#egr ) ]
     } grep { -f $_ } $dir->children(qr#\A$base-#);
     my $t = $targets[-1][0];
-    symlink( $t, $base );
+    push @actions, { 'base' => $base, 'target' => scalar( $t->basename() ), };
+}
+
+foreach my $rec (@actions)
+{
+    # body...
+    print "ln -sf $rec->{target} $rec->{base}\n";
+}
+print("\nIs it ok?\n");
+my $line = <>;
+chomp($line);
+
+if ( $line eq 'y' )
+{
+    foreach my $rec (@actions)
+    {
+        symlink( $rec->{target}, $rec->{base} );
+    }
 }
