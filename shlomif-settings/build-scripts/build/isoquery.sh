433777b1679dc54b2013ed70acc247902dfa1371
Shlomi Fish <shlomif@shlomifish.org>
2024-07-03 14:23:02 +0300

    add build+vcs scripts for some debian webwml deps

---
+++ shlomif-settings/build-scripts/build/isoquery.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# isoquery.sh
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+set -e -x
+./configure --prefix=$HOME/apps/wml-from-source
---
+++ shlomif-settings/vcs-checkout-scripts/scripts/debian-webwml.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# debian-webwml.sh
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+set -e -x
+git clone https://salsa.debian.org/webmaster-team/webwml
