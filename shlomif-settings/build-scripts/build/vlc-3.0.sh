75471022238a6d05b53e02dbfc02413cc5cc3813
Shlomi Fish <shlomif@shlomifish.org>
2024-09-23 07:58:07 +0300

    fix build script. vlc-3.0.

--- shlomif-settings/build-scripts/build/vlc-3.0.sh
+++ shlomif-settings/build-scripts/build/vlc-3.0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 p="/opt/vlc-3.0"
-export LUAC="/usr/bin/luac5.2"
+export LUAC="/usr/bin/luac"
 export CFLAGS='-g'
 export PKG_CONFIG_PATH="$p/lib/pkgconfig"
 ./configure --prefix="$p" --with-kde-solid="$p/share/apps/solid" --enable-debug
