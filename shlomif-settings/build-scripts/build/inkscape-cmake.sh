86f33e081a50d6c2510cb9ef0962e876cf2e31db
Shlomi Fish <shlomif@shlomifish.org>
2024-10-05 09:44:07 +0300

    code refactoring

--- shlomif-settings/build-scripts/build/inkscape-cmake.sh
+++ shlomif-settings/build-scripts/build/inkscape-cmake.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 build_type=''
-if test "$DEBUG" = 1 ; then
+if test "$DEBUG" = "1"
+then
     build_type='-DCMAKE_BUILD_TYPE=Debug'
 fi
-# flags=''
 
 ninja='-G Ninja'
-if test "$NO_NINJA" = 1 ; then
+if test "$NO_NINJA" = "1"
+then
     ninja=''
 fi
 
 prefix="$HOME/apps/graphics/inkscape-trunk"
 # prefix="/opt/`whoami`/inkscape-trunk"
-cmake $ninja $build_type -DCMAKE_INSTALL_PREFIX="$prefix" -DENABLE_LCMS=ON -DCMAKE_CXX_FLAGS="-fpermissive" -DENABLE_POPPLER_CAIRO=ON -DWITH_LIBSPELLING=OFF -DWITH_JEMALLOC=ON -DWITH_GRAPHICS_MAGICK=ON -DWITH_OPENMP=ON -DWITH_LIBCDR=ON -DBUILD_TESTING=ON $flags "$@"
+
+cmake \
+    $ninja \
+    $build_type \
+    -DBUILD_TESTING=ON \
+    -DCMAKE_CXX_FLAGS="-fpermissive" \
+    -DCMAKE_INSTALL_PREFIX="$prefix" \
+    -DENABLE_LCMS=ON \
+    -DENABLE_POPPLER_CAIRO=ON \
+    -DWITH_GRAPHICS_MAGICK=ON \
+    -DWITH_JEMALLOC=ON \
+    -DWITH_LIBCDR=ON \
+    -DWITH_LIBSPELLING=OFF \
+    -DWITH_OPENMP=ON \
+    $flags \
+    "$@"
