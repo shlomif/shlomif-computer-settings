852db6e70d55dc861cc9f551c37de0a94d71c4ed
Shlomi Fish <shlomif@shlomifish.org>
2024-10-11 18:32:36 +0300

    add vcs-checkout for CPAN mixin.pm

---
+++ shlomif-settings/vcs-checkout-scripts/scripts/perl-mixin.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# perl-mixin.sh
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+gh repo clone 'https://github.com/shlomif/mixin'
